8190b9793b1fc21753fe36b77c375183bedf5208
Rex Aliis <rexaliisdev@gmail.com>
2025-02-14 03:48:27 -0300

    remove unnecessary type parameter

--- src/Common/CheckHelper.ts
+++ src/Common/CheckHelper.ts
@@ -1,6 +1,6 @@
 ﻿export namespace CheckHelper
 {
-	export function isType<T>(object: T, type: any): boolean
+	export function isType(object: unknown, type: any): boolean
 	{
 		const isTypeOf = typeof type === "string" && typeof object === type;
 		const isInstanceOf = typeof type !== "string" && object instanceof type;
