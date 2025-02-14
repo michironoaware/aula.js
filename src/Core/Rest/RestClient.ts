3927b53a02d61ae500ffd226e12795e4f3d2f6c1
Rex Aliis <rexaliisdev@gmail.com>
2025-02-14 13:10:47 -0300

    remove rest client null token check

--- src/Core/Rest/RestClient.ts
+++ src/Core/Rest/RestClient.ts
@@ -11,7 +11,6 @@ import {AulaBadRequestError} from "./AulaBadRequestError.js";
 export class RestClient
 {
 	readonly #httpClient: HttpClient;
-	#token: string | null = null;
 
 	public constructor(
 		baseUri: URL | null = null,
@@ -59,11 +58,5 @@ export class RestClient
 		ThrowHelper.TypeError.throwIfNotType(value, "string");
 		this.#httpClient.defaultRequestHeaders.delete("Authorization");
 		this.#httpClient.defaultRequestHeaders.append("Authorization", `Bearer ${value}`);
-		this.#token = value;
-	}
-
-	#throwIfNullToken()
-	{
-		ThrowHelper.TypeError.throwIfNotType(this.#token, "string");
 	}
 }
