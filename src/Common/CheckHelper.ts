fec151999390dc8b34dc4a6a49c50e98cc18d849
Rex Aliis <rexaliisdev@gmail.com>
2025-02-14 03:07:29 -0300

    add helper methods for error throwing

---
+++ src/Common/CheckHelper.ts
@@ -0,0 +1,9 @@
+﻿export namespace CheckHelper
+{
+	export function isType<T>(object: T, type: any): boolean
+	{
+		const isTypeOf = typeof type === "string" && typeof object === type;
+		const isInstanceOf = typeof type !== "string" && object instanceof type;
+		return isTypeOf || isInstanceOf;
+	}
+}
---
+++ src/Common/ThrowHelper.ts
@@ -0,0 +1,43 @@
+﻿import {CheckHelper} from "./CheckHelper.js";
+
+const TypeErrorConstructor = TypeError;
+
+export namespace ThrowHelper
+{
+	export namespace TypeError
+	{
+		export function throwIfNull<T>(object: T): asserts object is T
+		{
+			if (object == null)
+			{
+				throw new TypeErrorConstructor(`Object is null.`);
+			}
+		}
+
+		export function throwIfNotType<T>(object: T, type: any): asserts object is T
+		{
+			if (!CheckHelper.isType(object, type))
+			{
+				throw new TypeErrorConstructor("Object is not an instance of the type expected.");
+			}
+		}
+
+		export function throwIfNotAnyType<T>(object: T, type: any[]): asserts object is T
+		{
+			const isAnyType = type.find(t => CheckHelper.isType(object, t)) != null;
+			if (!isAnyType)
+			{
+				throw new TypeErrorConstructor("Object is not an instance of the type expected.");
+			}
+		}
+
+		export function throwIfNotNullAndType<T>(object: T, type: any): asserts object is T
+		{
+			if (object != null &&
+				!CheckHelper.isType(object, type))
+			{
+				throw new TypeErrorConstructor("Object is not an instance of the type expected.");
+			}
+		}
+	}
+}
