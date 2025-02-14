631dacdf5ba15b891b0e3630dfd0eb709863dd1f
Rex Aliis <rexaliisdev@gmail.com>
2025-02-14 15:12:04 -0300

    add comments

--- src/Common/CheckHelper.ts
+++ src/Common/CheckHelper.ts
@@ -2,10 +2,14 @@
 {
 	export function isType(object: unknown, type: any): boolean
 	{
+		// Check whether typeof of the object is the same as the type string.
 		const isTypeOf = typeof type === "string" && typeof object === type;
-		const isInstanceOf = typeof type !== "string" && typeof type !== "object" && object instanceof type;
+
 		// For checking whether an enum value is inside the defined range
 		const isPropertyOf = typeof type === "object" && type[object as any];
+
+		// Check if the object is an instance of the specified constructor
+		const isInstanceOf = typeof type !== "string" && typeof type !== "object" && object instanceof type;
 
 		return isTypeOf || isInstanceOf || isPropertyOf;
 	}
