75b4a08c9541944f5850e4092aa4d5204e008730
Rex Aliis <rexaliisdev@gmail.com>
2025-02-14 15:10:30 -0300

    add typescript enum compatibility when type checking

--- src/Common/CheckHelper.ts
+++ src/Common/CheckHelper.ts
@@ -3,7 +3,10 @@
 	export function isType(object: unknown, type: any): boolean
 	{
 		const isTypeOf = typeof type === "string" && typeof object === type;
-		const isInstanceOf = typeof type !== "string" && object instanceof type;
-		return isTypeOf || isInstanceOf;
+		const isInstanceOf = typeof type !== "string" && typeof type !== "object" && object instanceof type;
+		// For checking whether an enum value is inside the defined range
+		const isPropertyOf = typeof type === "object" && type[object as any];
+
+		return isTypeOf || isInstanceOf || isPropertyOf;
 	}
 }
---
+++ src/Core/Rest/RestClientGetUsersOptions.ts
@@ -0,0 +1,15 @@
+﻿import {UserType} from "../Entities/UserType.js";
+
+export class RestClientGetUsersOptions
+{
+	static readonly #default = new RestClientGetUsersOptions();
+
+	static get default()
+	{
+		return RestClientGetUsersOptions.#default;
+	}
+
+	public type?: UserType;
+	public count?: number;
+	public after?: string;
+}
