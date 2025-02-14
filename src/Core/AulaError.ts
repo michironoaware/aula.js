588cda12e56a2c8a37e252a518c307c696947cf0
Rex Aliis <rexaliisdev@gmail.com>
2025-02-14 03:17:24 -0300

    add base error class

---
+++ src/Core/AulaError.ts
@@ -0,0 +1,17 @@
+﻿export default class AulaError extends Error
+{
+	readonly #content: string | null;
+
+	constructor(
+		message: string | undefined = undefined,
+		content: string | null)
+	{
+		super(message);
+		this.#content = content;
+	}
+
+	get content()
+	{
+		return this.#content;
+	}
+}
