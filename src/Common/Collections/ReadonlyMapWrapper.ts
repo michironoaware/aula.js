ea868f04dae710afbdf755b9801eda4f210ac0ff
Rex Aliis <rexaliisdev@gmail.com>
2025-02-14 03:06:51 -0300

    add readonly map wrapper

---
+++ src/Common/Collections/ReadonlyMapWrapper.ts
@@ -0,0 +1,50 @@
+export class ReadonlyMapWrapper<K, V> implements ReadonlyMap<K, V>
+{
+	readonly #underlyingMap: ReadonlyMap<K, V>;
+
+	public constructor(underlyingMap: ReadonlyMap<K, V>)
+	{
+		this.#underlyingMap = underlyingMap;
+	}
+
+	public get size()
+	{
+		return this.#underlyingMap.size;
+	}
+
+	public forEach(callbackfn: (value: V, key: K, map: ReadonlyMap<K, V>) => void, thisArg?: any): void
+	{
+		this.#underlyingMap.forEach(callbackfn);
+	}
+
+	public get(key: K)
+	{
+		return this.#underlyingMap.get(key);
+	}
+
+	public has(key: K)
+	{
+		return this.#underlyingMap.has(key);
+	}
+
+	public entries()
+	{
+		return this.#underlyingMap.entries();
+	}
+
+	public keys()
+	{
+		return this.#underlyingMap.keys();
+	}
+
+	public values()
+	{
+		return this.#underlyingMap.values();
+	}
+
+	public [Symbol.iterator]()
+	{
+		return this.#underlyingMap.entries();
+	}
+
+}
