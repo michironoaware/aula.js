1f1ea9b6330504d6cd50ef48b37391cc84a6c85a
Rex Aliis <rexaliisdev@gmail.com>
2025-02-23 17:02:38 -0300

    refactor HttpClient to use http message handlers

--- src/Common/Http/HttpClient.ts
+++ src/Common/Http/HttpClient.ts
@@ -1,11 +1,79 @@
 import {HttpRequestMessage} from "./HttpRequestMessage.js";
 import {HttpResponseMessage} from "./HttpResponseMessage.js";
 import {HeaderMap} from "./HeaderMap.js";
+import { HttpMessageHandler } from "./HttpMessageHandler.js";
+import { HttpFetchHandler } from "./HttpFetchHandler.js";
+import { ThrowHelper } from "../ThrowHelper.js";
+import { TypeHelper } from "../TypeHelper.js";
+import { InvalidOperationError } from "../InvalidOperationError.js";
 
-export abstract class HttpClient
+export class HttpClient
 {
-	abstract baseUri: URL | string | null;
-	abstract defaultRequestHeaders: HeaderMap;
+	#baseUri: URL | null = null;
+	#defaultRequestHeaders: HeaderMap = new HeaderMap();
+	#handler: HttpMessageHandler = new HttpFetchHandler();
+	
+	constructor()
+	{
+	}
+	public get baseUri()
+	{
+		return this.#baseUri;
+	}
+	
+	public set baseUri(value: URL | null)
+	{
+		ThrowHelper.TypeError.throwIfNotAnyType(value, URL, "null");
+		this.#baseUri = value;
+	}
+	
+	public get defaultRequestHeaders()
+	{
+		return this.#defaultRequestHeaders;
+	}
+	
+	public get handler()
+	{
+		return this.#handler;
+	}
+	
+	public async Send(message: HttpRequestMessage): Promise<HttpResponseMessage>
+	{
+		if (!(message.requestUri instanceof URL))
+		{
+			let requestUri : URL;
+			try
+			{
+				// Throws a TypeError if the uri is not absolute.
+				requestUri = new URL(message.requestUri);
+			}
+			catch (error)
+			{
+				if (!(error instanceof TypeError))
+				{
+					// Rethrow unhandled error type.
+					throw error;
+				}
 
-	abstract Send(message: HttpRequestMessage): Promise<HttpResponseMessage>;
+				if (this.#baseUri === null)
+				{
+					throw new InvalidOperationError("The HttpClient has no baseUri set, the request uri, cannot be relative.");
+				}
+
+				requestUri = new URL(message.requestUri, this.#baseUri.href);
+			}
+
+			message.requestUri = requestUri;
+		}
+
+		this.#defaultRequestHeaders.forEach((value, name) =>
+		{
+			if (!message.headers.has(name))
+            {
+                message.headers.append(name, value);
+            }
+		});
+		
+		return await this.#handler.Send(message);
+	}
 }
