89002b524c9fa5a79c55f1ed28780a9702f6e886
Rex Aliis <rexaliisdev@gmail.com>
2025-02-23 17:25:21 -0300

    set inner handler through the constructor

--- src/Common/Http/DelegatingHandler.ts
+++ src/Common/Http/DelegatingHandler.ts
@@ -1,12 +1,26 @@
+import { ThrowHelper } from "../ThrowHelper.js";
 import { HttpMessageHandler } from "./HttpMessageHandler.js";
 import { HttpRequestMessage } from "./HttpRequestMessage.js";
 
 export abstract class DelegatingHandler extends HttpMessageHandler
 {
-	abstract innerHandler: HttpMessageHandler;
+	readonly #innerHandler: HttpMessageHandler;
+
+	public constructor(innerHandler: HttpMessageHandler)
+	{
+		ThrowHelper.TypeError.throwIfNotType(innerHandler, HttpMessageHandler);
+		super();
+		
+		this.#innerHandler = innerHandler;
+	}
+
+	get innerHandler()
+	{
+		return this.#innerHandler;
+	}
 
 	async Send(message: HttpRequestMessage)
 	{
-		return await this.innerHandler.Send(message);
+		return await this.#innerHandler.Send(message);
 	}
 }
