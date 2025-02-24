79115b7d803b66440b3326a3d99bb4b89fa2c132
Rex Aliis <rexaliisdev@gmail.com>
2025-02-24 16:27:33 -0300

    add message entity class

---
+++ src/Core/Entities/Message.ts
@@ -0,0 +1,69 @@
+﻿import {RestClient} from "../Rest/RestClient.js";
+import {ThrowHelper} from "../../Common/ThrowHelper.js";
+import {MessageData} from "./Models/MessageData.js";
+import {Temporal} from "@js-temporal/polyfill";
+
+export class Message
+{
+	readonly #client: RestClient;
+	readonly #data: MessageData;
+
+	public constructor(client: RestClient, data: MessageData)
+	{
+		ThrowHelper.TypeError.throwIfNotType(client, RestClient);
+		ThrowHelper.TypeError.throwIfNotType(data, MessageData);
+
+		this.#client = client;
+		this.#data = data;
+	}
+
+	get id()
+	{
+		return this.#data.id;
+	}
+
+	get type()
+	{
+		return this.#data.type;
+	}
+
+	get flags()
+	{
+		return this.#data.flags;
+	}
+
+	get authorType()
+	{
+		return this.#data.authorType;
+	}
+
+	get authorId()
+	{
+		return this.#data.authorId;
+	}
+
+	get roomId()
+	{
+		return this.#data.roomId;
+	}
+
+	get content()
+	{
+		return this.#data.content;
+	}
+
+	get joinData()
+	{
+		return this.#data.joinData;
+	}
+
+	get leaveData()
+	{
+		return this.#data.leaveData;
+	}
+
+	get creationTime()
+	{
+		return Temporal.PlainDateTime.from(this.#data.creationTime);
+	}
+}
