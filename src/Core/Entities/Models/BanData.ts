f1c7a8bfac61785a85b613498e521233a263060c
Rex Aliis <rexaliisdev@gmail.com>
2025-02-24 20:09:50 -0300

    add ban dto model

---
+++ src/Core/Entities/Models/BanData.ts
@@ -0,0 +1,52 @@
+﻿import {BanType} from "./BanType.js";
+import {ThrowHelper} from "../../../Common/ThrowHelper.js";
+
+export class BanData
+{
+	readonly #type: BanType;
+	readonly #executorId: string | null;
+	readonly #reason: string | null;
+	readonly #targetId: string | null;
+	readonly #creationTime: string;
+
+	public constructor(data: any)
+	{
+		ThrowHelper.TypeError.throwIfNotType(data, "object");
+		ThrowHelper.TypeError.throwIfNotType(data.type, "number");
+		ThrowHelper.TypeError.throwIfNotAnyType(data.executorId, "string", "null", "undefined");
+		ThrowHelper.TypeError.throwIfNotAnyType(data.reason, "string", "null", "undefined");
+		ThrowHelper.TypeError.throwIfNotAnyType(data.targetId, "string", "null", "undefined");
+		ThrowHelper.TypeError.throwIfNotType(data.creationTime, "string");
+
+		this.#type = data.type;
+		this.#executorId = data.executorId;
+		this.#reason = data.reason;
+		this.#targetId = data.targetId;
+		this.#creationTime = data.creationTime;
+	}
+
+	public get type()
+	{
+		return this.#type;
+	}
+
+	public get executorId()
+	{
+		return this.#executorId;
+	}
+
+	public get reason()
+	{
+		return this.#reason;
+	}
+
+	public get targetId()
+	{
+		return this.#targetId;
+	}
+
+	public get creationTime()
+	{
+		return this.#creationTime;
+	}
+}
---
+++ src/Core/Entities/Models/BanType.ts
@@ -0,0 +1,4 @@
+﻿export enum BanType
+{
+	Id = 0,
+}
