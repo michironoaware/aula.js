85cf714a0d52a74bb5641b30ec05c0a9c2a43e12
Rex Aliis <rexaliisdev@gmail.com>
2025-02-24 15:53:23 -0300

    create RoomData model

---
+++ src/Core/Entities/Models/RoomData.ts
@@ -0,0 +1,59 @@
+﻿import {ThrowHelper} from "../../../Common/ThrowHelper.js";
+
+export class RoomData
+{
+	readonly #id: string;
+	readonly #name: string;
+	readonly #description: string | null;
+	readonly #isEntrance: boolean;
+	readonly #connectedRoomIds: ReadonlyArray<string>;
+	readonly #creationTime: string;
+
+	public constructor(data: any)
+	{
+		ThrowHelper.TypeError.throwIfNull(data);
+		ThrowHelper.TypeError.throwIfNotType(data.id, "string");
+		ThrowHelper.TypeError.throwIfNotType(data.name, "string");
+		ThrowHelper.TypeError.throwIfNotType(data.description, "string");
+		ThrowHelper.TypeError.throwIfNotType(data.isEntrance, "boolean");
+		ThrowHelper.TypeError.throwIfNotType(data.connectedRoomIds, Array<string>);
+		ThrowHelper.TypeError.throwIfNotType(data.creationTime, "string");
+
+		this.#id = data.id;
+		this.#name = data.name;
+		this.#description = data.description;
+		this.#isEntrance = data.isEntrance;
+		this.#connectedRoomIds = data.connectedRoomIds;
+		this.#creationTime = data.creationTime;
+	}
+
+	get id()
+	{
+		return this.#id;
+	}
+
+	get name()
+	{
+		return this.#name;
+	}
+
+	get description()
+	{
+		return this.#description;
+	}
+
+	get isEntrance()
+	{
+		return this.#isEntrance;
+	}
+
+	get connectedRoomIds()
+	{
+		return this.#connectedRoomIds;
+	}
+
+	get creationTime()
+	{
+		return this.#creationTime;
+	}
+}
