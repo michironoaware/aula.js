df5fb0f91d31ac320d311a000f757fae1ef32133
Rex Aliis <rexaliisdev@gmail.com>
2025-02-24 20:35:09 -0300

    add a object disposed error

---
+++ src/Common/ObjectDisposedError.ts
@@ -0,0 +1,19 @@
+﻿import {ThrowHelper} from "./ThrowHelper.js";
+
+export class ObjectDisposedError extends Error
+{
+	public constructor()
+	{
+		super("Cannot access a disposed object.");
+	}
+
+	public static throwIf(condition: boolean)
+	{
+		ThrowHelper.TypeError.throwIfNotType(condition, "boolean");
+
+		if (condition)
+		{
+			throw new ObjectDisposedError();
+		}
+	}
+}
