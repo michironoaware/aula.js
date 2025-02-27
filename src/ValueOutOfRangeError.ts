27e4987f22ddc2ba43d318b2bcb020ca3ab8894a
Rex Aliis <rexaliisdev@gmail.com>
2025-02-27 17:52:18 -0300

    create out of range error type

---
+++ src/ValueOutOfRangeError.ts
@@ -0,0 +1,78 @@
+﻿import {SealedClassError} from "./Common/SealedClassError.js";
+import {ThrowHelper} from "./Common/ThrowHelper.js";
+
+export class ValueOutOfRangeError extends Error
+{
+	public constructor(message: string, paramName?: string)
+	{
+		super(`Parameter name: "${paramName}".` + message ? ` ${message}` : "");
+		SealedClassError.throwIfNotEqual(ValueOutOfRangeError, new.target);
+		ThrowHelper.TypeError.throwIfNotType(paramName, "string");
+	}
+
+	public static throwIfEqual<T>(value: T, other: T, paramName?: string)
+	{
+		ThrowHelper.TypeError.throwIfNotType(value, typeof other);
+		ThrowHelper.TypeError.throwIfNotAnyType(paramName, "string", "undefined");
+
+		if (value === other)
+		{
+			throw new ValueOutOfRangeError((paramName ? `Parameter name: "${paramName}". ` : "") + "value is equal to other.");
+		}
+	}
+
+	public static throwIfGreaterThan<T>(value: T, other: T, paramName?: string)
+	{
+		ThrowHelper.TypeError.throwIfNotType(value, typeof other);
+		ThrowHelper.TypeError.throwIfNotAnyType(paramName, "string", "undefined");
+
+		if (value > other)
+		{
+			throw new ValueOutOfRangeError((paramName ? `Parameter name: "${paramName}". ` : "") + "value is greater than other.");
+		}
+	}
+
+	public static throwIfGreaterThanOrEqual<T>(value: T, other: T, paramName?: string)
+	{
+		ThrowHelper.TypeError.throwIfNotType(value, typeof other);
+		ThrowHelper.TypeError.throwIfNotAnyType(paramName, "string", "undefined");
+
+		if (value >= other)
+		{
+			throw new ValueOutOfRangeError((paramName ? `Parameter name: "${paramName}". ` : "") + "value is greater than or equal to other.");
+		}
+	}
+
+	public static throwIfLessThan<T>(value: T, other: T, paramName?: string)
+	{
+		ThrowHelper.TypeError.throwIfNotType(value, typeof other);
+		ThrowHelper.TypeError.throwIfNotAnyType(paramName, "string", "undefined");
+
+		if (value < other)
+		{
+			throw new ValueOutOfRangeError((paramName ? `Parameter name: "${paramName}". ` : "") + "value is less than other.");
+		}
+	}
+
+	public static throwIfLessThanOrEqual<T>(value: T, other: T, paramName?: string)
+	{
+		ThrowHelper.TypeError.throwIfNotType(value, typeof other);
+		ThrowHelper.TypeError.throwIfNotAnyType(paramName, "string", "undefined");
+
+		if (value <= other)
+		{
+			throw new ValueOutOfRangeError((paramName ? `Parameter name: "${paramName}". ` : "") + "value is less than or equal to other.");
+		}
+	}
+
+	public static throwIfNotEqual<T>(value: T, other: T, paramName?: string)
+	{
+		ThrowHelper.TypeError.throwIfNotType(value, typeof other);
+		ThrowHelper.TypeError.throwIfNotAnyType(paramName, "string", "undefined");
+
+		if (value !== other)
+		{
+			throw new ValueOutOfRangeError((paramName ? `Parameter name: "${paramName}". ` : "") + "value is not equal to other.");
+		}
+	}
+}
