0c379aa2c55c96ba9ab9b071eab775a53116a2a6
Rex Aliis <rexaliisdev@gmail.com>
2025-02-27 17:52:30 -0300

    create unreachable error

---
+++ src/Common/UnreachableError.ts
@@ -0,0 +1,11 @@
+﻿import {SealedClassError} from "./SealedClassError.js";
+import {InvalidOperationError} from "./InvalidOperationError.js";
+
+export class UnreachableError extends Error
+{
+	public constructor(message: string)
+	{
+		super(message);
+		SealedClassError.throwIfNotEqual(InvalidOperationError, new.target);
+	}
+}
