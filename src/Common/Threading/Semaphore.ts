7b1a74636024f07a80e7e4b5c551f89ad602a8e7
Rex Aliis <rexaliisdev@gmail.com>
2025-02-27 17:56:53 -0300

    add a Semaphore implementation

---
+++ src/Common/Threading/Semaphore.ts
@@ -0,0 +1,64 @@
+﻿import {ThrowHelper} from "../ThrowHelper.js";
+import {InvalidOperationError} from "../InvalidOperationError.js";
+import {ValueOutOfRangeError} from "../../ValueOutOfRangeError.js";
+import {SemaphoreFullError} from "./SemaphoreFullError.js";
+import {Action} from "../Action.js";
+import {SealedClassError} from "../SealedClassError.js";
+
+export class Semaphore {
+
+	readonly #queue: Action<[]>[] = [];
+	readonly #maximumCount: number;
+	#availableCount: number;
+
+	constructor(initialCount: number = 0, maximumCount: number = 1)
+	{
+		SealedClassError.throwIfNotEqual(Semaphore, new.target);
+		ThrowHelper.TypeError.throwIfNotType(initialCount, "number");
+		ThrowHelper.TypeError.throwIfNotType(maximumCount, "number");
+		ValueOutOfRangeError.throwIfLessThan(initialCount, 0);
+		ValueOutOfRangeError.throwIfLessThan(maximumCount, 1);
+		ValueOutOfRangeError.throwIfGreaterThan(initialCount, maximumCount);
+
+		this.#availableCount = initialCount;
+		this.#maximumCount = maximumCount;
+	}
+
+	public async waitOne()
+	{
+		return new Promise<void>(resolve =>
+		{
+			const tryGetLock = () =>
+			{
+				if(this.#availableCount > 0)
+				{
+					this.#availableCount--;
+					resolve();
+				}
+				else
+				{
+					this.#queue.push(tryGetLock);
+				}
+			}
+
+			tryGetLock();
+		});
+	}
+
+	public release(releaseCount: number = 1)
+	{
+		ThrowHelper.TypeError.throwIfNotType(releaseCount, "number");
+		ValueOutOfRangeError.throwIfLessThan(releaseCount, 1);
+
+		for (let i = 0; i < releaseCount; i++)
+		{
+			if (this.#availableCount === this.#maximumCount)
+			{
+				throw new SemaphoreFullError();
+			}
+
+			this.#availableCount += 1;
+			this.#queue.shift()?.();
+		}
+	}
+}
---
+++ src/Common/Threading/SemaphoreFullError.ts
@@ -0,0 +1,10 @@
+﻿import {SealedClassError} from "../SealedClassError.js";
+
+export class SemaphoreFullError extends Error
+{
+	public constructor()
+	{
+		super("The semaphore count is already at the maximum value.");
+		SealedClassError.throwIfNotEqual(SemaphoreFullError, new.target);
+	}
+}
