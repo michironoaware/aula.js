422f6fbe48c2008ab53912c9c19916498258cbd8
Rex Aliis <rexaliisdev@gmail.com>
2025-03-01 22:52:54 -0300

    Create AutoResetEvent.ts

---
+++ src/Common/Threading/AutoResetEvent.ts
@@ -0,0 +1,54 @@
+﻿import {ThrowHelper} from "../ThrowHelper.js";
+import {Action} from "../Action.js";
+import {SealedClassError} from "../SealedClassError.js";
+
+export class AutoResetEvent {
+
+	readonly #queue: Action<[]>[] = [];
+	#signaled: boolean;
+
+	constructor(initialState: boolean)
+	{
+		SealedClassError.throwIfNotEqual(AutoResetEvent, new.target);
+		ThrowHelper.TypeError.throwIfNotType(initialState, "boolean");
+
+		this.#signaled = initialState;
+	}
+
+	public async waitOne()
+	{
+		return new Promise<void>(resolve =>
+		{
+			const tryGetLock = () =>
+			{
+				if(this.#signaled)
+				{
+					this.#signaled = false;
+					resolve();
+				}
+				else
+				{
+					this.#queue.push(tryGetLock);
+				}
+			}
+
+			tryGetLock();
+		});
+	}
+
+	public set()
+	{
+		if (this.#signaled)
+		{
+			return;
+		}
+
+		this.#signaled = true;
+		this.#queue.shift()?.();
+	}
+
+	public reset()
+	{
+		this.#signaled = false;
+	}
+}
