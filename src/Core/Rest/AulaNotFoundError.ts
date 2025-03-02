d6803562a71d438ed57ee423b9fa280abb2e8443
Rex Aliis <rexaliisdev@gmail.com>
2025-03-02 17:47:35 -0300

    fix missing innerError parameter

--- src/Core/Rest/AulaNotFoundError.ts
+++ src/Core/Rest/AulaNotFoundError.ts
@@ -1,12 +1,13 @@
 ﻿import {AulaRestError} from "../AulaRestError.js";
 import {ThrowHelper} from "../../Common/ThrowHelper.js";
 import {SealedClassError} from "../../Common/SealedClassError.js";
+import {HttpRequestError} from "../../Common/Http/HttpRequestError.js";
 
 export class AulaNotFoundError extends AulaRestError
 {
-	public constructor(content: string | null)
+	public constructor(content: string | null, innerError: HttpRequestError | null)
 	{
-		super(`The resource doesn't exist.`, content);
+		super(`The resource doesn't exist.`, content, innerError);
 		SealedClassError.throwIfNotEqual(AulaNotFoundError, new.target);
 		ThrowHelper.TypeError.throwIfNotAnyType(content, "string", "null");
 	}
