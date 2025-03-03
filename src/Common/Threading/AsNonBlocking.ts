f1dadb0cb50a3e066d3276671e6d206402a22599
Rex Aliis <rexaliisdev@gmail.com>
2025-03-03 19:44:50 -0300

    Create AsNonBlocking.ts

---
+++ src/Common/Threading/AsNonBlocking.ts
@@ -0,0 +1,22 @@
+﻿import {Func} from "../Func.js";
+import {ThrowHelper} from "../ThrowHelper.js";
+
+export async function AsNonBlocking<TFunc extends Func<[], TReturn>, TReturn>(func: TFunc): Promise<TReturn>
+{
+	ThrowHelper.TypeError.throwIfNotType(func, "function");
+
+	return new Promise((resolve, reject) =>
+	{
+		setTimeout(() =>
+		{
+			try
+			{
+				resolve(func());
+			}
+			catch (error)
+			{
+				reject(error);
+			}
+		}, 0);
+	})
+}
