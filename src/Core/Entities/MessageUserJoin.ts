388e7dc5e1f18f8358d224672fbeb5f983d1c1fe
Rex Aliis <rexaliisdev@gmail.com>
2025-03-04 18:48:21 -0300

    add message user join entity

---
+++ src/Core/Entities/MessageUserJoin.ts
@@ -0,0 +1,35 @@
+﻿import { SealedClassError } from "../../Common/SealedClassError.js";
+import { MessageUserJoinData } from "./Models/MessageUserJoinData.js";
+import { ThrowHelper } from "../../Common/ThrowHelper.js";
+import { RestClient } from "../Rest/RestClient.js";
+
+export class MessageUserJoin
+{
+	readonly #restClient: RestClient;
+	readonly #data: MessageUserJoinData;
+
+	public constructor(restClient: RestClient, data: MessageUserJoinData)
+	{
+		SealedClassError.throwIfNotEqual(MessageUserJoin, new.target);
+		ThrowHelper.TypeError.throwIfNotType(restClient, RestClient);
+		ThrowHelper.TypeError.throwIfNotType(data, MessageUserJoinData);
+
+		this.#restClient = restClient;
+		this.#data = data;
+	}
+
+	public get restClient()
+	{
+		return this.#restClient;
+	}
+
+	public get userId()
+	{
+		return this.#data.userId;
+	}
+
+	public async getUser()
+	{
+		return await this.restClient.getUser(this.userId);
+	}
+}
