d2bfdd58cbed8440792b141dd9fd8ac4976f65d3
Rex Aliis <rexaliisdev@gmail.com>
2025-03-06 01:57:59 -0300

    return T[] instead of any[]

--- src/ArrayHelper.ts
+++ src/ArrayHelper.ts
@@ -5,6 +5,6 @@ export namespace ArrayHelper
 	export function asArray<T>(iterable: Iterable<T>)
 	{
 		ThrowHelper.TypeError.throwIfNotType(iterable, "iterable");
-		return Array.isArray(iterable) ? iterable : [ ...iterable ];
+		return Array.isArray(iterable) ? iterable as T[] : [ ...iterable ];
 	}
 }
