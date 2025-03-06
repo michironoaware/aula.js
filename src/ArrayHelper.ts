def39e2a64f2525dd44df6a2e8e498fd5793432e
Rex Aliis <rexaliisdev@gmail.com>
2025-03-06 01:22:49 -0300

    create array helper method

---
+++ src/ArrayHelper.ts
@@ -0,0 +1,10 @@
+﻿import { ThrowHelper } from "./Common/ThrowHelper.js";
+
+export namespace ArrayHelper
+{
+	export function asArray<T>(iterable: Iterable<T>)
+	{
+		ThrowHelper.TypeError.throwIfNotType(iterable, "iterable");
+		return Array.isArray(iterable) ? iterable : [ ...iterable ];
+	}
+}
