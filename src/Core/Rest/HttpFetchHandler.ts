2364d1ed6b961672b6804f235590204125b5d650
Rex Aliis <rexaliisdev@gmail.com>
2025-03-06 03:28:35 -0300

    move implementation out of http abstractions place

--- src/Core/Rest/HttpFetchHandler.ts
+++ src/Core/Rest/HttpFetchHandler.ts
@@ -1,14 +1,14 @@
-import { EmptyContent } from "./EmptyContent.js";
-import { HttpMessageHandler } from "./HttpMessageHandler.js";
-import { HttpMethod } from "./HttpMethod.js";
-import { HttpRequestMessage } from "./HttpRequestMessage.js";
-import { HttpResponseMessage } from "./HttpResponseMessage.js";
-import { HttpStatusCode } from "./HttpStatusCode.js";
-import { StreamContent } from "./StreamContent.js";
-import { SealedClassError } from "../SealedClassError.js";
-import { ThrowHelper } from "../ThrowHelper.js";
-import { HeaderMap } from "./HeaderMap.js";
-import { ObjectDisposedError } from "../ObjectDisposedError.js";
+import { EmptyContent } from "../../Common/Http/EmptyContent.js";
+import { HttpMessageHandler } from "../../Common/Http/HttpMessageHandler.js";
+import { HttpMethod } from "../../Common/Http/HttpMethod.js";
+import { HttpRequestMessage } from "../../Common/Http/HttpRequestMessage.js";
+import { HttpResponseMessage } from "../../Common/Http/HttpResponseMessage.js";
+import { HttpStatusCode } from "../../Common/Http/HttpStatusCode.js";
+import { StreamContent } from "../../Common/Http/StreamContent.js";
+import { SealedClassError } from "../../Common/SealedClassError.js";
+import { ThrowHelper } from "../../Common/ThrowHelper.js";
+import { HeaderMap } from "../../Common/Http/HeaderMap.js";
+import { ObjectDisposedError } from "../../Common/ObjectDisposedError.js";
 
 export class HttpFetchHandler extends HttpMessageHandler
 {
