7e1650c445ab21932104cc09ee151fa6e11988d7
Rex Aliis <rexaliisdev@gmail.com>
2025-03-08 18:20:33 -0300

    add isNotNullable type check method

--- src/Common/TypeHelper.ts
+++ src/Common/TypeHelper.ts
@@ -5,6 +5,11 @@
 		return object === null || object === undefined;
 	}
 
+	export function isNotNullable<T>(object: T): object is NonNullable<T>
+	{
+		return object !== null && object !== undefined;
+	}
+
 	export function isType<T extends TypeResolvable>(object: unknown, type: T): object is ResolvedType<T>
 	{
 		const isNullType = type === "null" && object === null;
