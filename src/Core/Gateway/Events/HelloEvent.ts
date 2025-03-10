2b6371adaeee73204f0b53918c3127718ee1e452
Rex Aliis <rexaliisdev@gmail.com>
2025-03-09 22:28:15 -0300

    add hello event entity class

---
+++ src/Core/Gateway/Events/HelloEvent.ts
@@ -0,0 +1,21 @@
+﻿import { SealedClassError } from "../../../Common/SealedClassError.js";
+import { HelloEventData } from "./Models/HelloEventData.js";
+import { ThrowHelper } from "../../../Common/ThrowHelper.js";
+
+export class HelloEvent
+{
+	readonly #data: HelloEventData;
+
+	public constructor(data: HelloEventData)
+	{
+		SealedClassError.throwIfNotEqual(HelloEvent, new.target);
+		ThrowHelper.TypeError.throwIfNotType(data, HelloEventData);
+
+		this.#data = data;
+	}
+
+	public get sessionId()
+	{
+		return this.#data.sessionId;
+	}
+}
