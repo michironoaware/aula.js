8d12afc8bbc9fc0128db495a18e3edfd20c98d27
Rex Aliis <rexaliisdev@gmail.com>
2025-03-09 22:26:57 -0300

    add hello event data dto

---
+++ src/Core/Gateway/Events/Models/HelloEventData.ts
@@ -0,0 +1,21 @@
+﻿import { SealedClassError } from "../../../../Common/SealedClassError.js";
+import { ThrowHelper } from "../../../../Common/ThrowHelper.js";
+
+export class HelloEventData
+{
+	readonly #sessionId: string;
+
+	public constructor(data: any)
+	{
+		SealedClassError.throwIfNotEqual(HelloEventData, new.target);
+		ThrowHelper.TypeError.throwIfNullable(data);
+		ThrowHelper.TypeError.throwIfNotType(data.sessionId, "string");
+
+		this.#sessionId = data.sessionId;
+	}
+
+	public get sessionId()
+	{
+		return this.#sessionId;
+	}
+}
