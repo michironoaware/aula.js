e7e58aa6bc52a99b6e78e816dc05ba65d024c38c
Rex Aliis <rexaliisdev@gmail.com>
2025-03-09 22:26:13 -0300

    add gateway operation types

---
+++ src/Core/Gateway/Events/OperationType.ts
@@ -0,0 +1,15 @@
+﻿/**
+ * The operation type of gateway payloads.
+ * */
+export enum OperationType
+{
+	/**
+	 * An event dispatch.
+	 * */
+	Dispatch = 0,
+
+	/**
+	 * Sent immediately after connecting, contains useful information for the client.
+	 * */
+	Hello = 1,
+}
