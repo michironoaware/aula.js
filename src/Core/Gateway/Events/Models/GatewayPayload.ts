0c7540a5a1d20d58c71da793ecbdcefeff854802
Rex Aliis <rexaliisdev@gmail.com>
2025-03-09 22:26:48 -0300

    add gateway payload data dto

---
+++ src/Core/Gateway/Events/Models/GatewayPayload.ts
@@ -0,0 +1,44 @@
+﻿import { SealedClassError } from "../../../../Common/SealedClassError.js";
+import { OperationType } from "../OperationType.js";
+import { EventType } from "../EventType.js";
+import { ThrowHelper } from "../../../../Common/ThrowHelper.js";
+import { TypeHelper } from "../../../../Common/TypeHelper.js";
+
+export class GatewayPayload<TData>
+{
+	readonly #operation: OperationType;
+	readonly #event: EventType;
+	readonly #eventData: TData | null = null;
+
+	public constructor(payloadData: any, eventDataTypeConstructor: new (arg1: any) => TData)
+	{
+		SealedClassError.throwIfNotEqual(GatewayPayload, new.target);
+		ThrowHelper.TypeError.throwIfNullable(payloadData);
+		ThrowHelper.TypeError.throwIfNotType(payloadData.operation, "number");
+		ThrowHelper.TypeError.throwIfNotType(payloadData.event, "number");
+		ThrowHelper.TypeError.throwIfNotAnyType(eventDataTypeConstructor, "function");
+
+		this.#operation = payloadData.operation;
+		this.#event = payloadData.event;
+
+		if (!TypeHelper.isNullable(payloadData.data))
+		{
+			this.#eventData = new eventDataTypeConstructor(payloadData.data);
+		}
+	}
+
+	public get operation()
+	{
+		return this.#operation;
+	}
+
+	public get event()
+	{
+		return this.#event;
+	}
+
+	public get data()
+	{
+		return this.#eventData;
+	}
+}
