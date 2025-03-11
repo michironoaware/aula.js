974a7aefc619f06f05424ed03d534468882221ee
Rex Aliis <rexaliisdev@gmail.com>
2025-03-11 19:59:32 -0300

    update docs to match RFC document

--- src/Common/WebSockets/WebSocketCloseCode.ts
+++ src/Common/WebSockets/WebSocketCloseCode.ts
@@ -4,67 +4,79 @@
 export enum WebSocketCloseCode
 {
 	/**
-	 * 1000: Normal Closure. The connection successfully completed its purpose.
-	 * */
+	 * Indicates a normal closure, meaning that the purpose for which the connection was established has been fulfilled.
+	 */
 	NormalClosure = 1000,
 
 	/**
-	 * 1001: Going Away. Indicates that an endpoint is "going away", such as a server shutting down or a browser navigating away from a page.
-	 * */
+	 * Indicates that an endpoint is "going away", such as a server going down or a browser having navigated away from a page.
+	 */
 	GoingAway = 1001,
 
 	/**
-	 * 1002: Protocol Error. Indicates that an endpoint is terminating the connection due to a protocol error.
-	 * */
+	 * Indicates that an endpoint is terminating the connection due to a protocol error.
+	 */
 	ProtocolError = 1002,
 
 	/**
-	 * 1003: Unsupported Data. Indicates that an endpoint is terminating the connection because it received data it cannot accept.
-	 * */
+	 * Indicates that an endpoint is terminating the connection because it has received a type of data it cannot accept
+	 * (e.g., an endpoint that understands only text data MAY send this if it receives a binary message).
+	 */
 	UnsupportedData = 1003,
 
 	/**
-	 * 1004: Reserved. This code is reserved for future use and should not be used in a Close frame.
-	 * */
+	 * Reserved. The specific meaning might be defined in the future.
+	 */
 	Reserved1004 = 1004,
 
 	/**
-	 * 1005: No Status Received. Reserved value indicating no status code was present. MUST NOT be used in a Close frame.
-	 * */
-	NoStatusReceived = 1005,
+	 * Is a reserved value and MUST NOT be set as a status code in a Close control frame by an endpoint
+	 * It is designated for use in applications expecting a status code to indicate that no status code was actually present.
+	 */
+	NoStatusCode = 1005,
 
 	/**
-	 * 1006: Abnormal Closure. Reserved value indicating that the connection was closed abnormally. MUST NOT be used in a Close frame.
-	 * */
+	 * Is a reserved value and MUST NOT be set as a status code in a Close control frame by an endpoint.
+	 * It is designated for use in applications expecting a status code to indicate that the connection was closed abnormally,
+	 * e.g., without sending or receiving a Close control frame.
+	 */
 	AbnormalClosure = 1006,
 
 	/**
-	 * 1007: Invalid Payload Data. Indicates that an endpoint is terminating the connection because it received data inconsistent with the message type (e.g., non-UTF-8 text data).
-	 * */
+	 * Indicates that an endpoint is terminating the connection because it has received data within a message
+	 * that was not consistent with the type of the message (e.g., non-UTF-8 [RFC3629] data within a text message).
+	 */
 	InvalidPayloadData = 1007,
 
 	/**
-	 * 1008: Policy Violation. Indicates that an endpoint is terminating the connection because it received a message that violates its policy.
+	 * indicates that an endpoint is terminating the connection because it has received a message that violates its policy.
+	 * This is a generic status code that can be returned when there is no other more suitable status code
+	 * (e.g., 1003 or 1009) or if there is a need to hide specific details about the policy.
 	 * */
 	PolicyViolation = 1008,
 
 	/**
-	 * 1009: Message Too Big. Indicates that an endpoint is terminating the connection because it received a message that is too large to handle.
+	 * Indicates that an endpoint is terminating the connection because it has received a message that is too big for it to process.
 	 * */
 	MessageTooBig = 1009,
 
 	/**
-	 * 1010: Missing Extension. Indicates that the client is terminating the connection because it expected the server to negotiate one or more extensions, but the server didn't return them.
-	 * */
+	 * Indicates that an endpoint (client) is terminating the connection because it has expected the server to
+	 * negotiate one or more extension, but the server didn't return them in the response message of the WebSocket handshake.
+	 * The list of extensions that are needed SHOULD appear in the /reason/ part of the Close frame.
+	 * Note that this status code is not used by the server, because it can fail the WebSocket handshake instead.
+	 */
 	MissingExtension = 1010,
 
 	/**
-	 * 1011: Internal Error. Indicates that a server is terminating the connection because it encountered an unexpected condition that prevented it from fulfilling the request.
+	 * Indicates that a server is terminating the connection because it encountered
+	 * an unexpected condition that prevented it from fulfilling the request.
 	 * */
 	InternalError = 1011,
 
 	/**
-	 * 1015: TLS Handshake Failure. Reserved value indicating that the connection was closed due to a failure in the TLS handshake. MUST NOT be used in a Close frame.
-	 * */
+	 * Reserved. Indicates that the connection was closed due to a failure to perform a TLS handshake
+	 * (e.g., the server certificate can't be verified).
+	 */
 	TlsHandshakeFailure = 1015,
 }
