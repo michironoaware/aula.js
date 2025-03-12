c4dbb38d46356a1ea9054588b0be03721c8e26a4
Rex Aliis <rexaliisdev@gmail.com>
2025-03-12 01:19:26 -0300

    Create PromiseCompletionSource.ts

---
+++ src/Common/Threading/PromiseCompletionSource.ts
@@ -0,0 +1,35 @@
+﻿import { Action } from "../Action.js";
+import { SealedClassError } from "../SealedClassError.js";
+
+export class PromiseCompletionSource<TResolve>
+{
+	readonly #_promise: Promise<TResolve>;
+	#_resolve: Action<[ TResolve ]> = null!;
+	#_reject: Action<[ any? ]> = null!;
+
+	public constructor()
+	{
+		SealedClassError.throwIfNotEqual(PromiseCompletionSource, new.target);
+
+		this.#_promise = new Promise<TResolve>((resolve, reject) =>
+		{
+			this.#_resolve = resolve;
+			this.#_reject = reject;
+		});
+	}
+
+	public get promise()
+	{
+		return this.#_promise;
+	}
+
+	public get resolve()
+	{
+		return this.#_resolve;
+	}
+
+	public get reject()
+	{
+		return this.#_reject;
+	}
+}
