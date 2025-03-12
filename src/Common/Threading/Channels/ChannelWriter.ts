48311f714804289877621e587d1e5de04983996b
Rex Aliis <rexaliisdev@gmail.com>
2025-03-12 01:56:59 -0300

    add complete method
    mark the channel as being complete, meaning no more items will be written to it.

--- src/Common/Threading/Channels/ChannelWriter.ts
+++ src/Common/Threading/Channels/ChannelWriter.ts
@@ -1,5 +1,7 @@
 ﻿export abstract class ChannelWriter<T>
 {
+	public abstract complete(): void;
+
 	public abstract waitToWrite(): Promise<boolean>;
 
 	public abstract write(item: T): Promise<void>;
