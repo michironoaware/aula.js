c7748993237b6ee3eea092f6619e145c64553fe3
Rex Aliis <rexaliisdev@gmail.com>
2025-03-13 01:51:59 -0300

    Create NotSupportedError.ts

---
+++ src/Common/NotSupportedError.ts
@@ -0,0 +1,12 @@
+﻿import { SealedClassError } from "./SealedClassError.js";
+import { ThrowHelper } from "./ThrowHelper.js";
+
+export class NotSupportedError extends Error
+{
+	public constructor(message: string = "The operation is not supported")
+	{
+		super(message);
+		SealedClassError.throwIfNotEqual(NotSupportedError, new.target);
+		ThrowHelper.TypeError.throwIfNotType(message, "string");
+	}
+}
