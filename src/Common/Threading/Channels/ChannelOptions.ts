32b2365cd40373ce45b1a1f5454ff3e2996de742
Rex Aliis <rexaliisdev@gmail.com>
2025-03-13 16:15:11 -0300

    Create ChannelOptions.ts

---
+++ src/Common/Threading/Channels/ChannelOptions.ts
@@ -0,0 +1,26 @@
+﻿import { ThrowHelper } from "../../ThrowHelper.js";
+
+export abstract class ChannelOptions
+{
+	readonly #_singleWriter: boolean;
+	readonly #_singleReader: boolean;
+
+	protected constructor(singleWriter: boolean, singleReader: boolean)
+	{
+		ThrowHelper.TypeError.throwIfNotType(singleWriter, "boolean");
+		ThrowHelper.TypeError.throwIfNotType(singleReader, "boolean");
+
+		this.#_singleWriter = singleWriter;
+		this.#_singleReader = singleReader;
+	}
+
+	public get singleReader()
+	{
+		return this.#_singleReader;
+	}
+
+	public get singleWriter()
+	{
+		return this.#_singleWriter;
+	}
+}
