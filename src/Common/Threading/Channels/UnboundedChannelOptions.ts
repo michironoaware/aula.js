c7c92626648f8b373c99952bfa48a06b9e871620
Rex Aliis <rexaliisdev@gmail.com>
2025-03-13 16:16:33 -0300

    Create UnboundedChannelOptions.ts

---
+++ src/Common/Threading/Channels/UnboundedChannelOptions.ts
@@ -0,0 +1,11 @@
+﻿import { ChannelOptions } from "./ChannelOptions.js";
+import { SealedClassError } from "../../SealedClassError.js";
+
+export class UnboundedChannelOptions extends ChannelOptions
+{
+	public constructor(singleWriter: boolean, singleReader: boolean)
+	{
+		super(singleWriter, singleReader);
+		SealedClassError.throwIfNotEqual(UnboundedChannelOptions, new.target);
+	}
+}
