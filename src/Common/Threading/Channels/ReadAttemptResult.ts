38eaac3264f8226b0a101fafa9ec1d2bde209c9d
Rex Aliis <rexaliisdev@gmail.com>
2025-03-13 16:50:30 -0300

    rename T type param to TItem

--- src/Common/Threading/Channels/ReadAttemptResult.ts
+++ src/Common/Threading/Channels/ReadAttemptResult.ts
@@ -1,12 +1,12 @@
 ﻿import { SealedClassError } from "../../SealedClassError.js";
 import { ThrowHelper } from "../../ThrowHelper.js";
 
-export class ReadAttemptResult<T>
+export class ReadAttemptResult<TItem>
 {
 	readonly #_succeeded: boolean;
-	readonly #_item: T;
+	readonly #_item: TItem;
 
-	public constructor(succeeded: boolean, item: T)
+	public constructor(succeeded: boolean, item: TItem)
 	{
 		SealedClassError.throwIfNotEqual(ReadAttemptResult, new.target);
 		ThrowHelper.TypeError.throwIfNotType(succeeded, "boolean");
