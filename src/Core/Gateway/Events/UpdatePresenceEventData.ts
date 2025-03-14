9bbf2581df3c7eb14696e9f82c3ce223e08ece34
Rex Aliis <rexaliisdev@gmail.com>
2025-03-14 18:52:30 -0300

    Create UpdatePresenceEventData.ts

---
+++ src/Core/Gateway/Events/UpdatePresenceEventData.ts
@@ -0,0 +1,19 @@
+﻿import { PresenceOptions } from "./PresenceOptions.js";
+import { ThrowHelper } from "../../../Common/ThrowHelper.js";
+
+export class UpdatePresenceEventData
+{
+	readonly #_presence: PresenceOptions;
+
+	public constructor(presence: PresenceOptions)
+	{
+		ThrowHelper.TypeError.throwIfNotType(presence, PresenceOptions);
+
+		this.#_presence = presence;
+	}
+
+	public get presence()
+	{
+		return this.#_presence;
+	}
+}
