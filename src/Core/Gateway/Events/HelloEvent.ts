da7f2f6dd24ef9b32c8a32dec7ebc3f36a49bb18
Rex Aliis <rexaliisdev@gmail.com>
2025-03-15 00:54:38 -0300

    add gateway client property

--- src/Core/Gateway/Events/HelloEvent.ts
+++ src/Core/Gateway/Events/HelloEvent.ts
@@ -1,21 +1,30 @@
 ﻿import { SealedClassError } from "../../../Common/SealedClassError.js";
 import { HelloOperationData } from "./Models/HelloOperationData.js";
 import { ThrowHelper } from "../../../Common/ThrowHelper.js";
+import { GatewayClient } from "../GatewayClient.js";
 
 export class HelloEvent
 {
 	readonly #data: HelloOperationData;
+	readonly #gatewayClient: GatewayClient;
 
-	public constructor(data: HelloOperationData)
+	public constructor(data: HelloOperationData, gatewayClient: GatewayClient)
 	{
 		SealedClassError.throwIfNotEqual(HelloEvent, new.target);
 		ThrowHelper.TypeError.throwIfNotType(data, HelloOperationData);
+		ThrowHelper.TypeError.throwIfNotType(gatewayClient, GatewayClient);
 
 		this.#data = data;
+		this.#gatewayClient = gatewayClient;
 	}
 
 	public get sessionId()
 	{
 		return this.#data.sessionId;
+	}
+
+	public get gatewayClient()
+	{
+		return this.#gatewayClient;
 	}
 }
