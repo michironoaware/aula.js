42eb5ec2f0983ce91cf9ad14313f133a91e961a8
Rex Aliis <rexaliisdev@gmail.com>
2025-03-15 00:14:32 -0300

    Create UserCurrentRoomUpdatedEventData.ts

---
+++ src/Core/Gateway/Events/Models/UserCurrentRoomUpdatedEventData.ts
@@ -0,0 +1,37 @@
+﻿import { SealedClassError } from "../../../../Common/SealedClassError.js";
+import { ThrowHelper } from "../../../../Common/ThrowHelper.js";
+
+export class UserCurrentRoomUpdatedEventData
+{
+	readonly #_userId: string;
+	readonly #_previousRoomId: string | null;
+	readonly #_currentRoomId: string | null;
+
+	public constructor(data: any)
+	{
+		SealedClassError.throwIfNotEqual(UserCurrentRoomUpdatedEventData, new.target);
+		ThrowHelper.TypeError.throwIfNullable(data);
+		ThrowHelper.TypeError.throwIfNotType(data.userId, "string");
+		ThrowHelper.TypeError.throwIfNotAnyType(data.previousRoomId, "string", "null", "undefined");
+		ThrowHelper.TypeError.throwIfNotAnyType(data.currentRoomId, "string", "null", "undefined");
+
+		this.#_userId = data.userId;
+		this.#_previousRoomId = data.previousRoomId ?? null;
+		this.#_currentRoomId = data.currentRoomId ?? null;
+	}
+
+	public get userId()
+	{
+		return this.#_userId;
+	}
+
+	public get previousRoomId()
+	{
+		return this.#_previousRoomId;
+	}
+
+	public get currentRoomId()
+	{
+		return this.#_currentRoomId;
+	}
+}
