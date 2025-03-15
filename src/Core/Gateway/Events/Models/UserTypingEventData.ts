b0a9fef85190d30de06ca5536fe4b9d24e788dc3
Rex Aliis <rexaliisdev@gmail.com>
2025-03-15 00:08:38 -0300

    Create UserTypingEventData.ts

---
+++ src/Core/Gateway/Events/Models/UserTypingEventData.ts
@@ -0,0 +1,29 @@
+﻿import { SealedClassError } from "../../../../Common/SealedClassError.js";
+import { ThrowHelper } from "../../../../Common/ThrowHelper.js";
+
+export class UserTypingEventData
+{
+	readonly #_userId: string;
+	readonly #_roomId: string;
+
+	public constructor(data: any)
+	{
+		SealedClassError.throwIfNotEqual(UserTypingEventData, new.target);
+		ThrowHelper.TypeError.throwIfNullable(data);
+		ThrowHelper.TypeError.throwIfNotType(data.userId, "string");
+		ThrowHelper.TypeError.throwIfNotType(data.roomId, "string");
+
+		this.#_userId = data.userId;
+		this.#_roomId = data.roomId;
+	}
+
+	public get userId()
+	{
+		return this.#_userId;
+	}
+
+	public get roomId()
+	{
+		return this.#_roomId;
+	}
+}
