7ebd73fba6c6ed4005437c670ae46480791238d1
Rex Aliis <rexaliisdev@gmail.com>
2025-03-15 00:32:04 -0300

    Create UserCurrentRoomUpdatedEvent.ts

---
+++ src/Core/Gateway/Events/UserCurrentRoomUpdatedEvent.ts
@@ -0,0 +1,31 @@
+﻿import { ThrowHelper } from "../../../Common/ThrowHelper.js";
+import { UserCurrentRoomUpdatedEventData } from "./Models/UserCurrentRoomUpdatedEventData.js";
+import { SealedClassError } from "../../../Common/SealedClassError.js";
+
+export class UserCurrentRoomUpdatedEvent
+{
+	readonly #_data: UserCurrentRoomUpdatedEventData;
+
+	public constructor(data: UserCurrentRoomUpdatedEventData)
+	{
+		SealedClassError.throwIfNotEqual(UserCurrentRoomUpdatedEvent, new.target);
+		ThrowHelper.TypeError.throwIfNotType(data, UserCurrentRoomUpdatedEventData);
+
+		this.#_data = data;
+	}
+
+	public get userId()
+	{
+		return this.#_data.userId;
+	}
+
+	public get previousRoomId()
+	{
+		return this.#_data.previousRoomId;
+	}
+
+	public get currentRoomId()
+	{
+		return this.#_data.currentRoomId;
+	}
+}
