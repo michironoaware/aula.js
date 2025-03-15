45062f8ba7f4f91d3f49906e98911b6a01d980c8
Rex Aliis <rexaliisdev@gmail.com>
2025-03-15 02:13:29 -0300

    remove gateway client property requirement

--- src/Core/Gateway/UpdatePresenceEvent.ts
+++ src/Core/Gateway/UpdatePresenceEvent.ts
@@ -1,30 +1,21 @@
 ﻿import { PresenceOption } from "./PresenceOption.js";
 import { ThrowHelper } from "../../Common/ThrowHelper.js";
 import { SealedClassError } from "../../Common/SealedClassError.js";
-import { GatewayClient } from "./GatewayClient.js";
 
 export class UpdatePresenceEvent
 {
 	readonly #_presence: PresenceOption;
-	readonly #_gatewayClient: GatewayClient;
 
-	public constructor(presence: PresenceOption, gatewayClient: GatewayClient)
+	public constructor(presence: PresenceOption)
 	{
 		SealedClassError.throwIfNotEqual(UpdatePresenceEvent, new.target);
 		ThrowHelper.TypeError.throwIfNotType(presence, PresenceOption);
-		ThrowHelper.TypeError.throwIfNotType(gatewayClient, GatewayClient);
 
 		this.#_presence = presence;
-		this.#_gatewayClient = gatewayClient;
 	}
 
 	public get presence()
 	{
 		return this.#_presence;
-	}
-
-	public get gatewayClient()
-	{
-		return this.#_gatewayClient;
 	}
 }
