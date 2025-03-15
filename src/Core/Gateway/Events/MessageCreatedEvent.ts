d6ca88185e9e92524e78377b3b43d1d77df0a1bd
Rex Aliis <rexaliisdev@gmail.com>
2025-03-15 01:07:17 -0300

    Create MessageCreatedEvent.ts

---
+++ src/Core/Gateway/Events/MessageCreatedEvent.ts
@@ -0,0 +1,30 @@
+﻿import { Message } from "../../Rest/Entities/Message.js";
+import { GatewayClient } from "../GatewayClient.js";
+import { SealedClassError } from "../../../Common/SealedClassError.js";
+import { ThrowHelper } from "../../../Common/ThrowHelper.js";
+
+export class MessageCreatedEvent
+{
+	readonly #_message: Message;
+	readonly #_gatewayClient: GatewayClient;
+
+	public constructor(message: Message, gatewayClient: GatewayClient)
+	{
+		SealedClassError.throwIfNotEqual(MessageCreatedEvent, new.target);
+		ThrowHelper.TypeError.throwIfNotType(message, Message);
+		ThrowHelper.TypeError.throwIfNotType(gatewayClient, GatewayClient);
+
+		this.#_message = message;
+		this.#_gatewayClient = gatewayClient;
+	}
+
+	public get message()
+	{
+		return this.#_message;
+	}
+
+	public get gatewayClient()
+	{
+		return this.#_gatewayClient;
+	}
+}
