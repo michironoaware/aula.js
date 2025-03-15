be89b06e9df3265e7600e78a7a06bf9cee31bbf0
Rex Aliis <rexaliisdev@gmail.com>
2025-03-15 00:18:16 -0300

    add missing event data cases

--- src/Core/Gateway/Events/Models/GatewayPayload.ts
+++ src/Core/Gateway/Events/Models/GatewayPayload.ts
@@ -3,12 +3,20 @@ import { OperationType } from "../OperationType.js";
 import { EventType } from "../EventType.js";
 import { ThrowHelper } from "../../../../Common/ThrowHelper.js";
 import { HelloOperationData } from "./HelloOperationData.js";
+import { BanData } from "../../../Rest/Entities/Models/BanData.js";
+import { MessageData } from "../../../Rest/Entities/Models/MessageData.js";
+import { UserTypingEventData } from "./UserTypingEventData.js";
+import { RoomConnectionEventData } from "./RoomConnectionEventData.js";
+import { RoomData } from "../../../Rest/Entities/Models/RoomData.js";
+import { UserCurrentRoomUpdatedEventData } from "./UserCurrentRoomUpdatedEventData.js";
+import { UserData } from "../../../Rest/Entities/Models/UserData.js";
 
 export class GatewayPayload
 {
 	readonly #operation: OperationType;
 	readonly #event: string | null;
-	readonly #data: HelloOperationData | null = null;
+	readonly #data: HelloOperationData | BanData | MessageData | UserTypingEventData | RoomConnectionEventData | RoomData |
+	                UserCurrentRoomUpdatedEventData | UserData | null = null;
 
 	public constructor(payloadData: any)
 	{
@@ -35,11 +43,41 @@ export class GatewayPayload
 
 				switch (this.#event)
 				{
-					// TODO:
+					case EventType[ EventType.BanCreated ]:
+					case EventType[ EventType.BanRemoved ]:
+						this.#data = new BanData(payloadData.data);
+						break;
+					case EventType[ EventType.MessageCreated ]:
+					case EventType[ EventType.MessageRemoved ]:
+						this.#data = new MessageData(payloadData.data);
+						break;
+					case EventType[ EventType.UserStartedTyping ]:
+					case EventType[ EventType.UserStoppedTyping ]:
+						this.#data = new UserTypingEventData(payloadData.data);
+						break;
+					case EventType[ EventType.RoomConnectionCreated ]:
+					case EventType[ EventType.RoomConnectionRemoved ]:
+						this.#data = new RoomConnectionEventData(payloadData.data);
+						break;
+					case EventType[ EventType.RoomCreated ]:
+					case EventType[ EventType.RoomUpdated ]:
+					case EventType[ EventType.RoomRemoved ]:
+						this.#data = new RoomData(payloadData.data);
+						break;
+					case EventType[ EventType.UserCurrentRoomUpdated ]:
+						this.#data = new UserCurrentRoomUpdatedEventData(payloadData.data);
+						break;
+					case EventType[ EventType.UserUpdated ]:
+						this.#data = new UserData(payloadData.data);
+						break;
+					default:
+						break;
 				}
 
 				break;
 			}
+			default:
+				break;
 		}
 	}
 
