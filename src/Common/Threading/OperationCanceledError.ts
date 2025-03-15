cf5ba60ba85dd84c49d12cc2be33d7da080aff1a
Rex Aliis <rexaliisdev@gmail.com>
2025-03-15 02:47:32 -0300

    seal class

--- src/Common/Threading/OperationCanceledError.ts
+++ src/Common/Threading/OperationCanceledError.ts
@@ -1,7 +1,10 @@
-﻿export class OperationCanceledError extends Error
+﻿import { SealedClassError } from "../SealedClassError.js";
+
+export class OperationCanceledError extends Error
 {
 	public constructor()
 	{
 		super("The operation was canceled.");
+		SealedClassError.throwIfNotEqual(OperationCanceledError, new.target);
 	}
 }
