259ba0ed2df5677496340c94b653ed2b8f13eeb7
Rex Aliis <rexaliisdev@gmail.com>
2025-03-15 00:59:43 -0300

    add a property to access the parent gateway client

--- src/Core/Gateway/Events/UpdatePresenceEvent.ts
+++ src/Core/Gateway/Events/UpdatePresenceEvent.ts
@@ -1,21 +1,30 @@
 ﻿import { PresenceOptions } from "./PresenceOptions.js";
 import { ThrowHelper } from "../../../Common/ThrowHelper.js";
 import { SealedClassError } from "../../../Common/SealedClassError.js";
+import { GatewayClient } from "../GatewayClient.js";
 
 export class UpdatePresenceEvent
 {
 	readonly #_presence: PresenceOptions;
+	readonly #_gatewayClient: GatewayClient;
 
-	public constructor(presence: PresenceOptions)
+	public constructor(presence: PresenceOptions, gatewayClient: GatewayClient)
 	{
 		SealedClassError.throwIfNotEqual(UpdatePresenceEvent, new.target);
 		ThrowHelper.TypeError.throwIfNotType(presence, PresenceOptions);
+		ThrowHelper.TypeError.throwIfNotType(gatewayClient, GatewayClient);
 
 		this.#_presence = presence;
+		this.#_gatewayClient = gatewayClient;
 	}
 
 	public get presence()
 	{
 		return this.#_presence;
+	}
+
+	public get gatewayClient()
+	{
+		return this.#_gatewayClient;
 	}
 }
