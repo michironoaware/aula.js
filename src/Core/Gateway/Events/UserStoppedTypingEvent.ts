dc509ea4a5b05d66e2c5dbd20e0a7c9c5e69add1
Rex Aliis <rexaliisdev@gmail.com>
2025-03-15 00:23:20 -0300

    Create UserStoppedTypingEvent.ts

---
+++ src/Core/Gateway/Events/UserStoppedTypingEvent.ts
@@ -0,0 +1,24 @@
+﻿import { UserTypingEventData } from "./Models/UserTypingEventData.js";
+import { ThrowHelper } from "../../../Common/ThrowHelper.js";
+
+export class UserStoppedTypingEvent
+{
+	readonly #_data: UserTypingEventData;
+
+	public constructor(data: UserTypingEventData)
+	{
+		ThrowHelper.TypeError.throwIfNotType(data, UserTypingEventData);
+
+		this.#_data = data;
+	}
+
+	public get userId()
+	{
+		return this.#_data.userId;
+	}
+
+	public get roomId()
+	{
+		return this.#_data.roomId;
+	}
+}
