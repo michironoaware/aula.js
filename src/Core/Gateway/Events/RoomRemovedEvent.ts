00f002036d782a02afb25e734a59d2885bec4e67
Rex Aliis <rexaliisdev@gmail.com>
2025-03-15 01:08:59 -0300

    Create RoomRemovedEvent.ts

---
+++ src/Core/Gateway/Events/RoomRemovedEvent.ts
@@ -0,0 +1,30 @@
+﻿import { Room } from "../../Rest/Entities/Room.js";
+import { GatewayClient } from "../GatewayClient.js";
+import { SealedClassError } from "../../../Common/SealedClassError.js";
+import { ThrowHelper } from "../../../Common/ThrowHelper.js";
+
+export class RoomRemovedEvent
+{
+	readonly #_room: Room;
+	readonly #_gatewayClient: GatewayClient;
+
+	public constructor(room: Room, gatewayClient: GatewayClient)
+	{
+		SealedClassError.throwIfNotEqual(RoomRemovedEvent, new.target);
+		ThrowHelper.TypeError.throwIfNotType(room, Room);
+		ThrowHelper.TypeError.throwIfNotType(gatewayClient, GatewayClient);
+
+		this.#_room = room;
+		this.#_gatewayClient = gatewayClient;
+	}
+
+	public get room()
+	{
+		return this.#_room;
+	}
+
+	public get gatewayClient()
+	{
+		return this.#_gatewayClient;
+	}
+}
