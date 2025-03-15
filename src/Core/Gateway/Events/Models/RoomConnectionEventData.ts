5c361272e56dbf8befb843614584f8d3f1ba3681
Rex Aliis <rexaliisdev@gmail.com>
2025-03-15 00:08:41 -0300

    Create RoomConnectionEventData.ts

---
+++ src/Core/Gateway/Events/Models/RoomConnectionEventData.ts
@@ -0,0 +1,29 @@
+﻿import { SealedClassError } from "../../../../Common/SealedClassError.js";
+import { ThrowHelper } from "../../../../Common/ThrowHelper.js";
+
+export class RoomConnectionEventData
+{
+	readonly #_sourceRoomId: string;
+	readonly #_targetRoomId: string;
+
+	public constructor(data: any)
+	{
+		SealedClassError.throwIfNotEqual(RoomConnectionEventData, new.target);
+		ThrowHelper.TypeError.throwIfNullable(data);
+		ThrowHelper.TypeError.throwIfNotType(data.sourceRoomId, "string");
+		ThrowHelper.TypeError.throwIfNotType(data.targetRoomId, "string");
+
+		this.#_sourceRoomId = data.sourceRoomId;
+		this.#_targetRoomId = data.targetRoomId;
+	}
+
+	public get sourceRoomId()
+	{
+		return this.#_sourceRoomId;
+	}
+
+	public get roomId()
+	{
+		return this.#_targetRoomId;
+	}
+}
