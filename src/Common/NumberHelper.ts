3a3f8f8076e8fae5762fb2324f96c5b1af004a73
Rex Aliis <rexaliisdev@gmail.com>
2025-03-15 21:39:23 -0300

    Add method to get the bit fields of a number

---
+++ src/Common/NumberHelper.ts
@@ -0,0 +1,22 @@
+﻿import { ThrowHelper } from "./ThrowHelper.js";
+
+export namespace NumberHelper
+{
+	export function getInt32BitFields(value: number): number[]
+	{
+		ThrowHelper.TypeError.throwIfNotType(value, "number");
+
+		const bitFields = [];
+
+		for (let i = 0; i < 32; i++)
+		{
+			const bit = 1 << i;
+			if ((value & bit) === bit)
+			{
+				bitFields.push(bit);
+			}
+		}
+
+		return bitFields;
+	}
+}
