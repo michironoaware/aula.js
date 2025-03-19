7c5ce0060adef86e31e51526777bf6838f13e956
Rex Aliis <rexaliisdev@gmail.com>
2025-03-19 16:03:58 -0300

    remove OperationType.Hello

--- src/Core/Gateway/Models/OperationType.ts
+++ src/Core/Gateway/Models/OperationType.ts
@@ -7,9 +7,4 @@ export enum OperationType
 	 * An event dispatch.
 	 * */
 	Dispatch = 0,
-
-	/**
-	 * Sent immediately after connecting, contains useful information for the client.
-	 * */
-	Hello = 1,
 }
