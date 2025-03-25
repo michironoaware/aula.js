5f389fbc9adc49527e0a9ffa6f6cd2e2cbe92fc0
Rex Aliis <rexaliisdev@gmail.com>
2025-03-25 20:12:46 -0300

    document ReadonlyMapWrapper

--- src/Common/Collections/ReadonlyMapWrapper.ts
+++ src/Common/Collections/ReadonlyMapWrapper.ts
@@ -1,10 +1,16 @@
 import { SealedClassError } from "../SealedClassError.js";
 import { ThrowHelper } from "../ThrowHelper.js";
 
+/**
+ * Represents a read-only collection of key/value pairs.
+ * */
 export class ReadonlyMapWrapper<TKey, TValue> implements ReadonlyMap<TKey, TValue>
 {
 	readonly #_underlyingMap: ReadonlyMap<TKey, TValue>;
 
+	/**
+	 * Initializes a new instance that it's wrapped around the specified map.
+	 * */
 	public constructor(underlyingMap: ReadonlyMap<TKey, TValue>)
 	{
 		SealedClassError.throwIfNotEqual(ReadonlyMapWrapper, new.target);
@@ -13,41 +19,65 @@ export class ReadonlyMapWrapper<TKey, TValue> implements ReadonlyMap<TKey, TValu
 		this.#_underlyingMap = underlyingMap;
 	}
 
+	/**
+	 * Gets the number of entries in the map.
+	 * */
 	public get size()
 	{
 		return this.#_underlyingMap.size;
 	}
 
+	/**
+	 * Executes the provided function for each entry in the map.
+	 * */
 	public forEach(callbackfn: (value: TValue, key: TKey, map: ReadonlyMap<TKey, TValue>) => void, thisArg?: any)
 	{
 		this.#_underlyingMap.forEach((value, key) => callbackfn(value, key, this), thisArg);
 	}
 
+	/**
+	 * Gets an item with the specified {@link key}.
+	 * */
 	public get(key: TKey)
 	{
 		return this.#_underlyingMap.get(key);
 	}
 
+	/**
+	 * Gets whether an entry with the specified {@link key} exists in the map.
+	 * */
 	public has(key: TKey)
 	{
 		return this.#_underlyingMap.has(key);
 	}
 
+	/**
+	 * Gets an iterator for the collection entries.
+	 * */
 	public entries()
 	{
 		return this.#_underlyingMap.entries();
 	}
 
+	/**
+	 * Gets an iterator for the collection keys.
+	 * */
 	public keys()
 	{
 		return this.#_underlyingMap.keys();
 	}
 
+	/**
+	 * Gets an iterator for the collection values.
+	 * */
 	public values()
 	{
 		return this.#_underlyingMap.values();
 	}
 
+	/**
+	 * Gets an iterator for the collection entries.
+	 * */
 	public [Symbol.iterator]()
 	{
 		return this.#_underlyingMap.entries();
