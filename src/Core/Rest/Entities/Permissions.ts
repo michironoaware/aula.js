bb55b42de0ead5b1fdd6b93c9441e82e92425e8e
Rex Aliis <rexaliisdev@gmail.com>
2025-03-25 15:46:50 -0300

    make Permissions a bigint enum

--- src/Core/Rest/Entities/Permissions.ts
+++ src/Core/Rest/Entities/Permissions.ts
@@ -1,12 +1,14 @@
-export enum Permissions
+export const Permissions =
 {
-	None = 0,
-	Administrator = 1 << 0,
-	ManageRooms = 1 << 1,
-	ReadMessages = 1 << 2,
-	SendMessages = 1 << 3,
-	ManageMessages = 1 << 4,
-	SetOwnCurrentRoom = 1 << 5,
-	SetCurrentRoom = 1 << 6,
-	BanUsers = 1 << 7,
+	None: 0n,
+	Administrator: 1n << 0n,
+	ManageRooms: 1n << 1n,
+	ReadMessages: 1n << 2n,
+	SendMessages: 1n << 3n,
+	ManageMessages: 1n << 4n,
+	SetOwnCurrentRoom: 1n << 5n,
+	SetCurrentRoom: 1n << 6n,
+	BanUsers: 1n << 7n,
 }
+
+export type Permissions = typeof Permissions[keyof typeof Permissions];
