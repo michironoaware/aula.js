730dfa9377d51ce0c099d2f4a6e6b8d6e90073fd
Rex Aliis <rexaliisdev@gmail.com>
2025-03-25 20:58:04 -0300

    document IDisposable

--- src/Common/IDisposable.ts
+++ src/Common/IDisposable.ts
@@ -1,4 +1,10 @@
-﻿export interface IDisposable
+﻿/**
+ * Provides a mechanism for releasing unmanaged resources.
+ * */
+export interface IDisposable
 {
+	/**
+	 * Performs application-defined tasks associated with freeing, releasing, or resetting resources.
+	 */
 	dispose(): void;
 }
