bf6adca0ca0df261799dc7ac3ddf6629269b82fd
Rex Aliis <rexaliisdev@gmail.com>
2025-03-26 09:21:10 -0300

    document DelegatingHandler

--- src/Common/Http/DelegatingHandler.ts
+++ src/Common/Http/DelegatingHandler.ts
@@ -2,10 +2,17 @@ import { ThrowHelper } from "../ThrowHelper.js";
 import { HttpMessageHandler } from "./HttpMessageHandler.js";
 import { HttpRequestMessage } from "./HttpRequestMessage.js";
 
+/**
+ * A type for HTTP handlers that delegate the processing of HTTP response messages to another handler, called the inner handler.
+ * */
 export abstract class DelegatingHandler extends HttpMessageHandler
 {
 	readonly #_innerHandler: HttpMessageHandler;
 
+	/**
+	 * Initializes a new instance of the {@link DelegatingHandler} class with a specific inner handler.
+	 * @param innerHandler The inner handler to delegate the processing of HTTP response messages.
+	 * */
 	protected constructor(innerHandler: HttpMessageHandler)
 	{
 		super();
@@ -14,6 +21,9 @@ export abstract class DelegatingHandler extends HttpMessageHandler
 		this.#_innerHandler = innerHandler;
 	}
 
+	/**
+	 * Gets the inner handler which processes the HTTP response messages.
+	 * */
 	public get innerHandler()
 	{
 		return this.#_innerHandler;
