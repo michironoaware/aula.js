853d90335966489a22b427fc4b9a9bcf75185e59
Rex Aliis <rexaliisdev@gmail.com>
2025-03-26 11:02:42 -0300

    document HttpResponseMessage

--- src/Common/Http/HttpResponseMessage.ts
+++ src/Common/Http/HttpResponseMessage.ts
@@ -6,12 +6,19 @@ import { ThrowHelper } from "../ThrowHelper.js";
 import { EmptyContent } from "./EmptyContent.js";
 import { HeaderMap } from "./HeaderMap.js";
 
+/**
+ * Represents a HTTP response message.
+ * */
 export class HttpResponseMessage
 {
 	#_statusCode: HttpStatusCode;
 	#_content: HttpContent = new EmptyContent();
 	#_headers: HeaderMap = new HeaderMap();
 
+	/**
+	 * Initializes a new instance of the {@link HttpResponseMessage} class with the specified status code, content and headers.
+	 * @param statusCode The status code of the HTTP response.
+	 * */
 	public constructor(statusCode: HttpStatusCode)
 	{
 		SealedClassError.throwIfNotEqual(HttpResponseMessage, new.target);
@@ -20,6 +27,9 @@ export class HttpResponseMessage
 		this.#_statusCode = statusCode;
 	}
 
+	/**
+	 * Gets or sets the status code of the HTTP response.
+	 * */
 	public get statusCode()
 	{
 		return this.#_statusCode;
@@ -31,6 +41,9 @@ export class HttpResponseMessage
 		this.#_statusCode = value;
 	}
 
+	/**
+	 * Gets or sets the content of a HTTP response message.
+	 * */
 	public get content()
 	{
 		return this.#_content;
@@ -42,16 +55,25 @@ export class HttpResponseMessage
 		this.#_content = value;
 	}
 
+	/**
+	 * Gets the collection of HTTP response headers.
+	 * */
 	public get headers()
 	{
 		return this.#_headers;
 	}
 
+	/**
+	 * Gets a value that indicates if the HTTP response was successful.
+	 * */
 	public get isSuccessStatusCode()
 	{
 		return this.#_statusCode >= 200 && this.#_statusCode < 300;
 	}
 
+	/**
+	 * Throws an exception if the {@link isSuccessStatusCode} property for the {@link HttpResponseMessage} is false.
+	 * */
 	public ensureSuccessStatusCode()
 	{
 		if (!this.isSuccessStatusCode)
