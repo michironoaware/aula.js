41462edb4830a164c7ec119b831b5c52ca50e0f5
Rex Aliis <rexaliisdev@gmail.com>
2025-03-26 09:45:33 -0300

    Create PrivateConstructorError.ts

---
+++ src/Common/PrivateConstructorError.ts
@@ -0,0 +1,31 @@
+﻿import { ThrowHelper } from "./ThrowHelper.js";
+
+/**
+ * Thrown when a private constructor is called outside its class.
+ * */
+export class PrivateConstructorError extends Error
+{
+	/**
+	 * Initializes a new instance of {@link PrivateConstructorError}.
+	 * */
+	public constructor()
+	{
+		super("Cannot call a private constructor from outside the class.");
+	}
+
+	/**
+	 * Throws if the {@link providedKey} is not equal to the {@link constructorKey}.
+	 * @param constructorKey A secret symbol that only the class has access to.
+	 * @param providedKey The symbol to compare against the {@link constructorKey}.
+	 * */
+	public static throwIfNotEqual(constructorKey: symbol, providedKey: symbol)
+	{
+		ThrowHelper.TypeError.throwIfNotType(constructorKey, "symbol");
+		ThrowHelper.TypeError.throwIfNotType(providedKey, "symbol");
+
+		if (constructorKey !== providedKey)
+		{
+			throw new PrivateConstructorError();
+		}
+	}
+}
