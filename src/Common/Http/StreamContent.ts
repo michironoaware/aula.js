4e9d79c0ddf780cf29caadd54a7f979ed93758db
Rex Aliis <rexaliisdev@gmail.com>
2025-03-25 21:27:38 -0300

    document StreamContent

--- src/Common/Http/StreamContent.ts
+++ src/Common/Http/StreamContent.ts
@@ -4,12 +4,20 @@ import { HeaderMap } from "./HeaderMap.js";
 import { SealedClassError } from "../SealedClassError.js";
 import { ObjectDisposedError } from "../ObjectDisposedError.js";
 
+/**
+ * Provides HTTP content based on a stream.
+ * */
 export class StreamContent extends HttpContent
 {
 	readonly #_stream: ReadableStream<Uint8Array>;
 	readonly #_headers: HeaderMap;
 	#_disposed: boolean = false;
 
+	/**
+	 * Initializes a new instance of {@link StreamContent}.
+	 * @param stream The underlying stream of content.
+	 * @param contentType The media type of the content as defined in {@link https://www.rfc-editor.org/rfc/rfc6838 RFC 6836}.
+	 * */
 	public constructor(stream: ReadableStream<Uint8Array>, contentType = "application/octet-stream")
 	{
 		super();
