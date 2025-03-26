2f1143d3ce1e939a85d1cc96d12b0cb3223aab8d
Rex Aliis <rexaliisdev@gmail.com>
2025-03-26 10:05:53 -0300

    add helper method for throwing InvalidOperationError

--- src/Common/InvalidOperationError.ts
+++ src/Common/InvalidOperationError.ts
@@ -7,4 +7,20 @@ export class InvalidOperationError extends Error
 		super(message);
 		ThrowHelper.TypeError.throwIfNotType(message, "string");
 	}
+
+	/**
+	 * Throws an {@link InvalidOperationError} if the specified condition is true.
+	 * @param condition The condition to evaluate.
+	 * @param message The message of the error.
+	 * */
+	public static throwIf(condition: boolean, message: string): asserts condition is false
+	{
+		ThrowHelper.TypeError.throwIfNotType(condition, "boolean");
+		ThrowHelper.TypeError.throwIfNotType(message, "string");
+
+		if (condition)
+		{
+			throw new InvalidOperationError(message);
+		}
+	}
 }
