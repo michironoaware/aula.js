87b157d8c7db4d68a94acf334f78567938bf3c95
Rex Aliis <rexaliisdev@gmail.com>
2025-03-26 10:30:39 -0300

    document HttpRequestError

--- src/Common/Http/HttpRequestError.ts
+++ src/Common/Http/HttpRequestError.ts
@@ -2,10 +2,19 @@
 import { ThrowHelper } from "../ThrowHelper.js";
 import { SealedClassError } from "../SealedClassError.js";
 
+/**
+ * A base class for exceptions thrown by the {@link HttpClient} and {@link HttpMessageHandler} classes.
+ * */
 export class HttpRequestError extends Error
 {
 	readonly #_statusCode: HttpStatusCode | null;
 
+	/**
+	 * Initializes a new instance of the {@link HttpRequestError} class
+	 * with a specific message that describes the current error and a statusCode.
+	 * @param message A message that describes the current error.
+	 * @param statusCode The status code of the HTTP response that caused the error.
+	 * */
 	public constructor(message: string | null = null, statusCode: HttpStatusCode | null = null)
 	{
 		SealedClassError.throwIfNotEqual(HttpRequestError, new.target);
@@ -16,6 +25,9 @@ export class HttpRequestError extends Error
 		this.#_statusCode = statusCode;
 	}
 
+	/**
+	 * Gets the {@link HttpStatusCode} of the HTTP response that caused the error.
+	 * */
 	public get statusCode()
 	{
 		return this.#_statusCode;
