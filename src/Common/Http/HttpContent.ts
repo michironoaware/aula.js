e33b9d3cc4e951101765b8e005678d10bca54326
Rex Aliis <rexaliisdev@gmail.com>
2025-03-26 10:52:51 -0300

    add reference to RFC 2616

--- src/Common/Http/HttpContent.ts
+++ src/Common/Http/HttpContent.ts
@@ -7,7 +7,7 @@ import { IDisposable } from "../IDisposable.js";
 export abstract class HttpContent implements IDisposable
 {
 	/**
-	 * Gets the HTTP content headers as defined in RFC 2616.
+	 * Gets the HTTP content headers as defined in {@link https://www.rfc-editor.org/rfc/rfc2616 RFC 2616}.
 	 * */
 	public abstract get headers(): HeaderMap;
 
