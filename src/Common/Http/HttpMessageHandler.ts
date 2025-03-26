5e038e1908a71cd8d476b17f8a055e02ce804c55
Rex Aliis <rexaliisdev@gmail.com>
2025-03-25 21:58:22 -0300

    document HttpMessageHandler

--- src/Common/Http/HttpMessageHandler.ts
+++ src/Common/Http/HttpMessageHandler.ts
@@ -2,8 +2,16 @@ import { HttpRequestMessage } from "./HttpRequestMessage.js";
 import { HttpResponseMessage } from "./HttpResponseMessage.js";
 import { IDisposable } from "../IDisposable.js";
 
+/**
+ * A base type for HTTP message handlers. Responsible for processing HTTP messages.
+ * */
 export abstract class HttpMessageHandler implements IDisposable
 {
+	/**
+	 * Send an HTTP request as an asynchronous operation.
+	 * @returns The promise object representing the asynchronous operation.
+	 * @remarks The returned {@link Promise} will complete once the response has been received.
+	 * */
 	public abstract send(message: HttpRequestMessage): Promise<HttpResponseMessage>;
 
 	public abstract dispose(): void;
