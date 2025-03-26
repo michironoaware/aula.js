0d73e594402f7c240dae2a153d007c5a3075ba5f
Rex Aliis <rexaliisdev@gmail.com>
2025-03-26 00:28:41 -0300

    StringContent no longer throws ObjectDisposedError

--- src/Common/Http/StringContent.ts
+++ src/Common/Http/StringContent.ts
@@ -1,13 +1,11 @@
 ﻿import { HttpContent } from "./HttpContent.js";
 import { ThrowHelper } from "../ThrowHelper.js";
 import { HeaderMap } from "./HeaderMap.js";
-import { ObjectDisposedError } from "../ObjectDisposedError.js";
 
 export class StringContent extends HttpContent
 {
 	readonly #_headers: HeaderMap;
 	readonly #_string: string;
-	#_disposed: boolean = false;
 
 	public constructor(stringValue: string, contentType: string = "text/plain")
 	{
@@ -22,24 +20,20 @@ export class StringContent extends HttpContent
 
 	public get headers()
 	{
-		ObjectDisposedError.throwIf(this.#_disposed);
 		return this.#_headers;
 	}
 
 	public readAsStream()
 	{
-		ObjectDisposedError.throwIf(this.#_disposed);
 		return new Blob([ this.#_string ]).stream();
 	}
 
 	public readAsString()
 	{
-		ObjectDisposedError.throwIf(this.#_disposed);
 		return Promise.resolve(this.#_string);
 	}
 
 	public dispose()
 	{
-		this.#_disposed = true;
 	}
 }
