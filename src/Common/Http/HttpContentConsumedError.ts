5e89b0b08c298ea2d4e6fa1622b909af5f72e5c9
Rex Aliis <rexaliisdev@gmail.com>
2025-03-26 00:24:53 -0300

    add static helper method

--- src/Common/Http/HttpContentConsumedError.ts
+++ src/Common/Http/HttpContentConsumedError.ts
@@ -1,5 +1,6 @@
 ﻿import { InvalidOperationError } from "../InvalidOperationError.js";
 import { SealedClassError } from "../SealedClassError.js";
+import { ThrowHelper } from "../ThrowHelper.js";
 
 export class HttpContentConsumedError extends InvalidOperationError
 {
@@ -7,5 +8,15 @@ export class HttpContentConsumedError extends InvalidOperationError
 	{
 		super("Cannot consume already read content.");
 		SealedClassError.throwIfNotEqual(HttpContentConsumedError, new.target);
+	}
+
+	public static throwIf(condition: boolean): asserts condition is false
+	{
+		ThrowHelper.TypeError.throwIfNotType(condition, "boolean");
+
+		if (condition)
+		{
+			throw new HttpContentConsumedError();
+		}
 	}
 }
