65a8b5c0496606f4d13b3e51507ebe7ec99aad83
Rex Aliis <rexaliisdev@gmail.com>
2025-03-26 00:22:10 -0300

    Create HttpContentConsumedError.ts

---
+++ src/Common/Http/HttpContentConsumedError.ts
@@ -0,0 +1,11 @@
+﻿import { InvalidOperationError } from "../InvalidOperationError.js";
+import { SealedClassError } from "../SealedClassError.js";
+
+export class HttpContentConsumedError extends InvalidOperationError
+{
+	public constructor()
+	{
+		super("Cannot consume already read content.");
+		SealedClassError.throwIfNotEqual(HttpContentConsumedError, new.target);
+	}
+}
