3b80cfbe5c18f5ebd32667afa3f3e65f6aba1963
Rex Aliis <rexaliisdev@gmail.com>
2025-03-29 20:49:38 -0300

    Create ByteArrayContent.ts

---
+++ src/Common/Http/ByteArrayContent.ts
@@ -0,0 +1,55 @@
+﻿import { HeaderMap } from "./HeaderMap.js";
+import { HttpContent } from "./HttpContent.js";
+import { ThrowHelper } from "../ThrowHelper.js";
+
+/**
+ * Provides HTTP content based on a byte array.
+ * */
+export class ByteArrayContent extends HttpContent
+{
+	static readonly #s_textDecoder: TextDecoder = new TextDecoder(undefined, { fatal: true });
+	readonly #_headers: HeaderMap = new HeaderMap();
+	readonly #_byteArray: Uint8Array;
+
+	/**
+	 * Initializes a new instance of {@link ByteArrayContent}
+	 * @param bytes The byte array with the content.
+	 * @param contentType The media type of the content as defined in {@link https://www.rfc-editor.org/rfc/rfc6838 RFC 6836}.
+	 * */
+	public constructor(bytes: Uint8Array, contentType: string = "application/octet-stream")
+	{
+		super();
+		ThrowHelper.TypeError.throwIfNotType(bytes, Uint8Array);
+		ThrowHelper.TypeError.throwIfNotType(contentType, "string");
+
+		this.#_byteArray = bytes;
+		this.#_headers.append("Content-Type", contentType);
+	}
+
+	public get headers()
+	{
+		return this.#_headers;
+	}
+
+	public readAsStream()
+	{
+		const byteArray = this.#_byteArray;
+		return new ReadableStream<Uint8Array>({
+			
+			start(controller)
+			{
+				controller.enqueue(byteArray);
+				controller.close();
+			},
+		});
+	}
+
+	public readAsString()
+	{
+		return Promise.resolve(ByteArrayContent.#s_textDecoder.decode(this.#_byteArray));
+	}
+
+	public dispose()
+	{
+	}
+}
