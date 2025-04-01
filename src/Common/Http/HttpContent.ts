393af9138a535e19038e5dc8394e80924ee94f91
Rex Aliis <rexaliisdev@gmail.com>
2025-04-01 19:51:20 -0300

    implement read methods in base class

--- src/Common/Http/HttpContent.ts
+++ src/Common/Http/HttpContent.ts
@@ -1,11 +1,14 @@
 import { HeaderMap } from "./HeaderMap.js";
 import { IDisposable } from "../IDisposable.js";
+import { UInt8Stream } from "../IO/UInt8Stream.js";
 
 /**
  * A base class representing an HTTP entity body and content headers.
  * */
 export abstract class HttpContent implements IDisposable
 {
+	static #s_textDecoder = new TextDecoder(undefined, { fatal: true });
+
 	/**
 	 * Gets the HTTP content headers as defined in {@link https://www.rfc-editor.org/rfc/rfc2616 RFC 2616}.
 	 * */
@@ -19,12 +22,34 @@ export abstract class HttpContent implements IDisposable
 	/**
 	 * Serialize the HTTP content to a byte array as an asynchronous operation.
 	 * */
-	public abstract readAsByteArray(): Promise<Uint8Array>;
+	public async readAsByteArray(): Promise<Uint8Array>
+	{
+		const contentReader = (await this.readAsStream()).getReader();
+		const stringBytes = new UInt8Stream(256);
+		const stringWriter = stringBytes.getWriter();
+
+		while (true)
+		{
+			const { done, value } = await contentReader.read();
+			await stringWriter.write(value);
+
+			if (done)
+			{
+				await stringWriter.close();
+				break;
+			}
+		}
+
+		return stringBytes.written;
+	}
 
 	/**
 	 * Serialize the HTTP content to a string as an asynchronous operation.
 	 * */
-	public abstract readAsString(): Promise<string>;
+	public async readAsString(): Promise<string>
+	{
+		return HttpContent.#s_textDecoder.decode(await this.readAsByteArray());
+	}
 
 	public abstract dispose(): void;
 }
