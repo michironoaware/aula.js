7be4c7d9be2594b4573b8503187ef40c69418f57
Rex Aliis <rexaliisdev@gmail.com>
2025-04-01 16:00:31 -0300

    remove paramName parameter

--- src/Common/ValueOutOfRangeError.ts
+++ src/Common/ValueOutOfRangeError.ts
@@ -3,83 +3,76 @@ import { ThrowHelper } from "./ThrowHelper.js";
 
 export class ValueOutOfRangeError extends Error
 {
-	public constructor(message: string, paramName?: string)
+	public constructor(message: string)
 	{
-		super(`Parameter name: "${paramName}"` + message ? `. ${message}` : "");
+		super(message);
 		SealedClassError.throwIfNotEqual(ValueOutOfRangeError, new.target);
 		ThrowHelper.TypeError.throwIfNotType(message, "string");
-		ThrowHelper.TypeError.throwIfNotType(paramName, "string");
 	}
 
-	public static throwIfEqual(value: number, other: number, paramName?: string)
+	public static throwIfEqual(value: number, other: number)
 	{
 		ThrowHelper.TypeError.throwIfNotType(value, "number");
 		ThrowHelper.TypeError.throwIfNotType(other, "number");
-		ThrowHelper.TypeError.throwIfNotAnyType(paramName, "string", "undefined");
 
 		if (value === other)
 		{
-			throw new ValueOutOfRangeError((paramName ? `Parameter name: "${paramName}". ` : "") + "Value is equal to other");
+			throw new ValueOutOfRangeError("Value is equal to other");
 		}
 	}
 
-	public static throwIfGreaterThan(value: number, other: number, paramName?: string)
+	public static throwIfGreaterThan(value: number, other: number)
 	{
 		ThrowHelper.TypeError.throwIfNotType(value, "number");
 		ThrowHelper.TypeError.throwIfNotType(other, "number");
-		ThrowHelper.TypeError.throwIfNotAnyType(paramName, "string", "undefined");
 
 		if (value > other)
 		{
-			throw new ValueOutOfRangeError((paramName ? `Parameter name: "${paramName}". ` : "") + "value is greater than other");
+			throw new ValueOutOfRangeError("value is greater than other");
 		}
 	}
 
-	public static throwIfGreaterThanOrEqual(value: number, other: number, paramName?: string)
+	public static throwIfGreaterThanOrEqual(value: number, other: number)
 	{
 		ThrowHelper.TypeError.throwIfNotType(value, "number");
 		ThrowHelper.TypeError.throwIfNotType(other, "number");
-		ThrowHelper.TypeError.throwIfNotAnyType(paramName, "string", "undefined");
 
 		if (value >= other)
 		{
-			throw new ValueOutOfRangeError((paramName ? `Parameter name: "${paramName}". ` : "") + "value is greater than or equal to other");
+			throw new ValueOutOfRangeError("value is greater than or equal to other");
 		}
 	}
 
-	public static throwIfLessThan(value: number, other: number, paramName?: string)
+	public static throwIfLessThan(value: number, other: number)
 	{
 		ThrowHelper.TypeError.throwIfNotType(value, "number");
 		ThrowHelper.TypeError.throwIfNotType(other, "number");
-		ThrowHelper.TypeError.throwIfNotAnyType(paramName, "string", "undefined");
 
 		if (value < other)
 		{
-			throw new ValueOutOfRangeError((paramName ? `Parameter name: "${paramName}". ` : "") + "value is less than other");
+			throw new ValueOutOfRangeError("value is less than other");
 		}
 	}
 
-	public static throwIfLessThanOrEqual(value: number, other: number, paramName?: string)
+	public static throwIfLessThanOrEqual(value: number, other: number)
 	{
 		ThrowHelper.TypeError.throwIfNotType(value, "number");
 		ThrowHelper.TypeError.throwIfNotType(other, "number");
-		ThrowHelper.TypeError.throwIfNotAnyType(paramName, "string", "undefined");
 
 		if (value <= other)
 		{
-			throw new ValueOutOfRangeError((paramName ? `Parameter name: "${paramName}". ` : "") + "value is less than or equal to other");
+			throw new ValueOutOfRangeError("value is less than or equal to other");
 		}
 	}
 
-	public static throwIfNotEqual(value: number, other: number, paramName?: string)
+	public static throwIfNotEqual(value: number, other: number)
 	{
 		ThrowHelper.TypeError.throwIfNotType(value, "number");
 		ThrowHelper.TypeError.throwIfNotType(other, "number");
-		ThrowHelper.TypeError.throwIfNotAnyType(paramName, "string", "undefined");
 
 		if (value !== other)
 		{
-			throw new ValueOutOfRangeError((paramName ? `Parameter name: "${paramName}". ` : "") + "value is not equal to other");
+			throw new ValueOutOfRangeError("value is not equal to other");
 		}
 	}
 }
