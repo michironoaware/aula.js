f9c17ed5bb20123245e997aec530ef0168edb15f
Rex Aliis <rexaliisdev@gmail.com>
2025-04-01 21:24:47 -0300

    Create StringHelper.ts

---
+++ src/Common/StringHelper.ts
@@ -0,0 +1,9 @@
+﻿export namespace StringHelper
+{
+	const whitespaceChars = " \t\n\r\v\f";
+
+	export function isWhiteSpace(str: string)
+	{
+		return str.length === 0 || Array.from(str).every(c => whitespaceChars.includes(c));
+	}
+}
