99e56031568cebe761ab8ea91cdc393975a764ac
Rex Aliis <rexaliisdev@gmail.com>
2025-04-01 21:26:37 -0300

    Create MultipartFormDataContent.ts

---
+++ src/Common/Http/MultipartFormDataContent.ts
@@ -0,0 +1,24 @@
+﻿import { MultipartContent } from "./MultipartContent.js";
+import { ThrowHelper } from "../ThrowHelper.js";
+import { HttpContent } from "./HttpContent.js";
+
+export class MultipartFormDataContent extends MultipartContent
+{
+	public constructor(boundary?: string)
+	{
+		super("form-data", boundary);
+	}
+
+	public add(content: HttpContent, name?: string, filename?: string)
+	{
+		ThrowHelper.TypeError.throwIfNotType(content, HttpContent);
+		ThrowHelper.TypeError.throwIfNotType(name, "string");
+		ThrowHelper.TypeError.throwIfNotAnyType(filename, "string", "undefined");
+
+		content.headers.add("Content-Disposition",
+			`form-data`
+			+ (name ? `; name="${name}"` : "")
+			+ (filename ? `; filename="${filename}"` : ""));
+		super.add(content);
+	}
+}
