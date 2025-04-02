5bd9415dd3e530c810145df363d5943cfd4f60a2
Rex Aliis <rexaliisdev@gmail.com>
2025-04-02 01:57:16 -0300

    remove unnecessary disposing logic

--- src/Core/Rest/AulaHttpStatusCode503Handler.ts
+++ src/Core/Rest/AulaHttpStatusCode503Handler.ts
@@ -3,14 +3,11 @@ import { DelegatingHandler } from "../../Common/Http/DelegatingHandler.js";
 import { HttpMessageHandler } from "../../Common/Http/HttpMessageHandler.js";
 import { HttpRequestMessage } from "../../Common/Http/HttpRequestMessage.js";
 import { ThrowHelper } from "../../Common/ThrowHelper.js";
-import { ObjectDisposedError } from "../../Common/ObjectDisposedError.js";
 import { HttpStatusCode } from "../../Common/Http/HttpStatusCode.js";
 import { HttpResponseMessage } from "../../Common/Http/HttpResponseMessage.js";
 
 export class AulaHttpStatusCode503Handler extends DelegatingHandler
 {
-	#_disposed: boolean = false;
-
 	public constructor(innerHandler: HttpMessageHandler)
 	{
 		super(innerHandler);
@@ -20,7 +17,6 @@ export class AulaHttpStatusCode503Handler extends DelegatingHandler
 	public async send(message: HttpRequestMessage)
 	{
 		ThrowHelper.TypeError.throwIfNotType(message, HttpRequestMessage);
-		ObjectDisposedError.throwIf(this.#_disposed);
 
 		let response: HttpResponseMessage;
 		do
@@ -33,11 +29,5 @@ export class AulaHttpStatusCode503Handler extends DelegatingHandler
 
 	public dispose()
 	{
-		if (this.#_disposed)
-		{
-			return;
-		}
-
-		this.#_disposed = true;
 	}
 }
