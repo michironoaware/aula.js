7e7f046d98c836a0475b9cf44ec3d48002b01714
Rex Aliis <rexaliisdev@gmail.com>
2025-04-02 01:57:54 -0300

    add retry delay for 503 responses

--- src/Core/Rest/AulaHttpStatusCode503Handler.ts
+++ src/Core/Rest/AulaHttpStatusCode503Handler.ts
@@ -5,6 +5,7 @@ import { HttpRequestMessage } from "../../Common/Http/HttpRequestMessage.js";
 import { ThrowHelper } from "../../Common/ThrowHelper.js";
 import { HttpStatusCode } from "../../Common/Http/HttpStatusCode.js";
 import { HttpResponseMessage } from "../../Common/Http/HttpResponseMessage.js";
+import { Delay } from "../../Common/Threading/Delay.js";
 
 export class AulaHttpStatusCode503Handler extends DelegatingHandler
 {
@@ -18,9 +19,24 @@ export class AulaHttpStatusCode503Handler extends DelegatingHandler
 	{
 		ThrowHelper.TypeError.throwIfNotType(message, HttpRequestMessage);
 
+		let retryDelayMilliseconds = 0;
 		let response: HttpResponseMessage;
 		do
 		{
+			if (retryDelayMilliseconds !== 0)
+			{
+				await Delay(retryDelayMilliseconds);
+			}
+			else
+			{
+				retryDelayMilliseconds = 100;
+			}
+
+			if (retryDelayMilliseconds < 51_200)
+			{
+				retryDelayMilliseconds *= 2;
+			}
+
 			response = await super.send(message);
 		} while (response.statusCode === HttpStatusCode.InternalServerError);
 
