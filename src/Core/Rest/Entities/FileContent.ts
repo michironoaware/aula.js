66ff54828ee39e892aee48c3aa96242930a414da
Rex Aliis <rexaliisdev@gmail.com>
2025-04-02 02:19:19 -0300

    implement IDisposable for FileContent

--- src/Core/Rest/Entities/FileContent.ts
+++ src/Core/Rest/Entities/FileContent.ts
@@ -1,10 +1,13 @@
 ﻿import { HttpContent } from "../../../Common/Http/HttpContent.js";
 import { ThrowHelper } from "../../../Common/ThrowHelper.js";
 import { SealedClassError } from "../../../Common/SealedClassError.js";
+import { IDisposable } from "../../../Common/IDisposable.js";
+import { ObjectDisposedError } from "../../../Common/ObjectDisposedError.js";
 
-export class FileContent
+export class FileContent implements IDisposable
 {
 	readonly #_httpContent: HttpContent;
+	#_disposed: boolean = false;
 
 	public constructor(httpContent: HttpContent)
 	{
@@ -16,11 +19,24 @@ export class FileContent
 
 	public async readAsStream()
 	{
+		ObjectDisposedError.throwIf(this.#_disposed);
 		return await this.#_httpContent.readAsStream();
 	}
 
 	public async readAsByteArray()
 	{
+		ObjectDisposedError.throwIf(this.#_disposed);
 		return await this.#_httpContent.readAsByteArray();
+	}
+
+	public dispose()
+	{
+		if (this.#_disposed)
+		{
+			return;
+		}
+
+		this.#_httpContent.dispose();
+		this.#_disposed = true;
 	}
 }
