6c22f254371127626721682d028832ec3793fe2e
Rex Aliis <rexaliisdev@gmail.com>
2025-04-02 01:34:00 -0300

    Create File.ts

---
+++ src/Core/Rest/Entities/FileContent.ts
@@ -0,0 +1,26 @@
+﻿import { HttpContent } from "../../../Common/Http/HttpContent.js";
+import { ThrowHelper } from "../../../Common/ThrowHelper.js";
+import { SealedClassError } from "../../../Common/SealedClassError.js";
+
+export class FileContent
+{
+	readonly #_httpContent: HttpContent;
+
+	public constructor(httpContent: HttpContent)
+	{
+		SealedClassError.throwIfNotEqual(FileContent, new.target);
+		ThrowHelper.TypeError.throwIfNotType(httpContent, HttpContent);
+
+		this.#_httpContent = httpContent;
+	}
+
+	public async readAsStream()
+	{
+		return this.#_httpContent.readAsStream();
+	}
+
+	public async readAsByteArray()
+	{
+		return this.#_httpContent.readAsByteArray();
+	}
+}
