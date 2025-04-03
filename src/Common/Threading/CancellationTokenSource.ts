0efb43eec552120bc4e47f9bfe07cce4213a3acb
Rex Aliis <rexaliisdev@gmail.com>
2025-04-03 03:04:20 -0300

    add "Cancelled" event emitter to cancellation token sources

--- src/Common/Threading/CancellationTokenSource.ts
+++ src/Common/Threading/CancellationTokenSource.ts
@@ -1,10 +1,14 @@
 ﻿import { SealedClassError } from "../SealedClassError.js";
 import { CancellationToken } from "./CancellationToken.js";
+import { EventEmitter } from "./EventEmitter.js";
+import { Func } from "../Func.js";
+import { ThrowHelper } from "../ThrowHelper.js";
 
 export class CancellationTokenSource
 {
 	#_cancellationRequested: boolean = false;
 	#_token: CancellationToken | null = null;
+	#_eventEmitter: EventEmitter<ICancellationTokenSourceEvents> | null = null;
 
 	public constructor()
 	{
@@ -23,6 +27,35 @@ export class CancellationTokenSource
 
 	public cancel()
 	{
+		if (this.#_cancellationRequested)
+		{
+			return;
+		}
+
 		this.#_cancellationRequested = true;
+		this.#_eventEmitter?.emit("Cancelled");
 	}
+
+	public on<TEvent extends keyof ICancellationTokenSourceEvents>(
+		event: TEvent,
+		listener: ICancellationTokenSourceEvents[TEvent])
+	{
+		ThrowHelper.TypeError.throwIfNullable(event);
+		ThrowHelper.TypeError.throwIfNotType(listener, "function");
+
+		// If operation is already cancelled call listener immediately.
+		if (this.#_cancellationRequested &&
+		    event == "Cancelled")
+		{
+			listener();
+			return;
+		}
+
+		(this.#_eventEmitter ??= new EventEmitter()).on(event, listener);
+	}
+}
+
+export interface ICancellationTokenSourceEvents
+{
+	Cancelled: Func<[]>;
 }
