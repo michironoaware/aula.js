c6f95e94bddb1b9e550157e732a9b14f26300405
Rex Aliis <rexaliisdev@gmail.com>
2025-04-05 06:34:08 -0300

    document RoomData

--- src/Core/Rest/Entities/Models/RoomData.ts
+++ src/Core/Rest/Entities/Models/RoomData.ts
@@ -1,6 +1,10 @@
 ﻿import { ThrowHelper } from "../../../../Common/ThrowHelper.js";
 import { SealedClassError } from "../../../../Common/SealedClassError.js";
 
+/**
+ * Provides a strongly typed DTO class for the API v1 RoomData JSON schema.
+ * @package
+ * */
 export class RoomData
 {
 	readonly #_id: string;
@@ -12,6 +16,11 @@ export class RoomData
 	readonly #_connectedRoomIds: ReadonlyArray<string>;
 	readonly #_creationDate: string;
 
+	/**
+	 * Initializes a new instance of {@link RoomData}.
+	 * @param data An object that conforms to the API v1 RoomData JSON schema
+	 *             from where the data will be extracted.
+	 * */
 	public constructor(data: any)
 	{
 		SealedClassError.throwIfNotEqual(RoomData, new.target);
@@ -38,41 +47,66 @@ export class RoomData
 		this.#_creationDate = data.creationDate;
 	}
 
+	/**
+	 * The id of the room.
+	 * */
 	public get id()
 	{
 		return this.#_id;
 	}
 
+	/**
+	 * The type of the room.
+	 * */
 	public get type()
 	{
 		return this.#_type;
 	}
 
+	/**
+	 * The name of the room.
+	 * */
 	public get name()
 	{
 		return this.#_name;
 	}
 
+	/**
+	 * The description of the room.
+	 * */
 	public get description()
 	{
 		return this.#_description;
 	}
 
+	/**
+	 * Whether a user can enter this room without coming from any other room.
+	 * */
 	public get isEntrance()
 	{
 		return this.#_isEntrance;
 	}
 
+	/**
+	 * The file id of the background audio associated with this room.
+	 * */
 	public get backgroundAudioId()
 	{
 		return this.#_backgroundAudioId;
 	}
 
+	/**
+	 * A collection of ids of the rooms that a user can travel to from this room.
+	 * */
 	public get connectedRoomIds()
 	{
 		return this.#_connectedRoomIds;
 	}
 
+	/**
+	 * Gets the creation date of the room,
+	 * expressed as a {@link https://en.wikipedia.org/wiki/ISO_8601 ISO 8601} string.
+	 * */
 	public get creationDate()
 	{
 		return this.#_creationDate;
