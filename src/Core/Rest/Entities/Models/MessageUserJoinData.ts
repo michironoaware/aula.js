d7dcacbe6fa08fd9e32546936468171c00553716
Rex Aliis <rexaliisdev@gmail.com>
2025-04-05 00:06:51 -0300

    document MessageUserJoinData

--- src/Core/Rest/Entities/Models/MessageUserJoinData.ts
+++ src/Core/Rest/Entities/Models/MessageUserJoinData.ts
@@ -1,10 +1,19 @@
 ﻿import { ThrowHelper } from "../../../../Common/ThrowHelper.js";
 import { SealedClassError } from "../../../../Common/SealedClassError.js";
 
+/**
+ * Provides a strongly typed DTO class for the API v1 MessageUserJoinData JSON schema.
+ * @package
+ * */
 export class MessageUserJoinData
 {
 	readonly #_userId: string;
 
+	/**
+	 * Initializes a new instance of {@link MessageUserJoinData}.
+	 * @param data An object that conforms to the API v1 MessageUserJoinData JSON schema
+	 *             from where the data will be extracted.
+	 * */
 	public constructor(data: any)
 	{
 		SealedClassError.throwIfNotEqual(MessageUserJoinData, new.target);
@@ -14,6 +23,9 @@ export class MessageUserJoinData
 		this.#_userId = data.userId;
 	}
 
+	/**
+	 * The id of the user who joined the room.
+	 * */
 	public get userId()
 	{
 		return this.#_userId;
