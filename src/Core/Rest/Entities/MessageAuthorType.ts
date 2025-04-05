fa8c59f0360b9e44023fb16be38828791dccba87
Rex Aliis <rexaliisdev@gmail.com>
2025-04-05 20:42:03 -0300

    rewrite to avoid use of ts enums

--- src/Core/Rest/Entities/MessageAuthorType.ts
+++ src/Core/Rest/Entities/MessageAuthorType.ts
@@ -1,5 +1,12 @@
-﻿export enum MessageAuthorType
+﻿export const MessageAuthorType: {
+	User: 0,
+	System: 1,
+} =
 {
-	User = 0,
-	System = 1,
+	User: 0,
+	System: 1,
 }
+
+Object.freeze(MessageAuthorType);
+
+export type MessageAuthorType = typeof MessageAuthorType[keyof typeof MessageAuthorType]; 
