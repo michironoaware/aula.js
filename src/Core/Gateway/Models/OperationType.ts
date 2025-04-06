f68cfa6b5b74a815601c2708f966ab2be192cf45
Rex Aliis <rexaliisdev@gmail.com>
2025-04-05 21:21:42 -0300

    rewrite OperationType to avoid use of ts enums

--- src/Core/Gateway/Models/OperationType.ts
+++ src/Core/Gateway/Models/OperationType.ts
@@ -1,10 +1,14 @@
 ﻿/**
  * The operation type of gateway payloads.
  * */
-export enum OperationType
+export const OperationType =
 {
 	/**
 	 * An event dispatch.
 	 * */
-	Dispatch = 0,
-}
+	Dispatch: 0,
+} as const;
+
+Object.freeze(OperationType);
+
+export type OperationType = typeof OperationType[keyof typeof OperationType];
