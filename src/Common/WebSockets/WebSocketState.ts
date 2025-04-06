39f3746c96a48181b5eddbab295f80e0cbffe638
Rex Aliis <rexaliisdev@gmail.com>
2025-04-05 21:28:20 -0300

    rewrite WebSocketState to avoid use of ts enums

--- src/Common/WebSockets/WebSocketState.ts
+++ src/Common/WebSockets/WebSocketState.ts
@@ -1,27 +1,31 @@
-﻿export enum WebSocketState
+﻿export const WebSocketState =
 {
 	/**
 	 * The connection is negotiating the handshake with the remote endpoint.
 	 * */
-	Connecting = 1,
+	Connecting: 1,
 
 	/**
 	 * The initial state after the HTTP handshake has been completed.
 	 * */
-	Open = 2,
+	Open: 2,
 
 	/**
 	 * A close message was sent to the remote endpoint.
 	 * */
-	CloseSent = 3,
+	CloseSent: 3,
 
 	/**
 	 * A close message was received from the remote endpoint.
 	 * */
-	CloseReceived = 4,
+	CloseReceived: 4,
 
 	/**
 	 * Indicates the WebSocket close handshake completed gracefully.
 	 * */
-	Closed = 5,
-}
+	Closed: 5,
+} as const;
+
+Object.freeze(WebSocketState);
+
+export type WebSocketState = typeof WebSocketState[keyof typeof WebSocketState];
