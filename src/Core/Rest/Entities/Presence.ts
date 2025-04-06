9c5de4bbcde3ac5565692bb67f0827ed00d1c81e
Rex Aliis <rexaliisdev@gmail.com>
2025-04-05 21:06:30 -0300

    rewrite Presence to avoid use of ts enums

--- src/Core/Rest/Entities/Presence.ts
+++ src/Core/Rest/Entities/Presence.ts
@@ -1,5 +1,12 @@
-export enum Presence
+export const Presence: {
+	Offline: 0,
+	Online: 1,
+} =
 {
-	Offline = 0,
-	Online = 1,
+	Offline: 0,
+	Online: 1,
 }
+
+Object.freeze(Presence);
+
+export type Presence = typeof Presence[keyof typeof Presence];
