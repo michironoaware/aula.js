5a13901565152d5bb21d8288a8e7bd9d3e15b955
Rex Aliis <rexaliisdev@gmail.com>
2025-04-05 21:07:32 -0300

    rewrite PresenceOption to avoid use of ts enums

--- src/Core/Gateway/PresenceOption.ts
+++ src/Core/Gateway/PresenceOption.ts
@@ -1,5 +1,12 @@
-﻿export enum PresenceOption
+﻿export const PresenceOption: {
+	Invisible: 0,
+	Online: 1,
+} =
 {
-	Invisible = 0,
-	Online = 1,
+	Invisible: 0,
+	Online: 1,
 }
+
+Object.freeze(PresenceOption);
+
+export type PresenceOption = typeof PresenceOption[keyof typeof PresenceOption];
