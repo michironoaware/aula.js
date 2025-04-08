fe22929ad3190aa68cfb8508884f4598c272e134
Rex Aliis <rexaliisdev@gmail.com>
2025-04-08 19:12:28 -0300

    document ISendMessageRequestBody

--- src/Core/Rest/ISendMessageRequestBody.ts
+++ src/Core/Rest/ISendMessageRequestBody.ts
@@ -1,4 +1,8 @@
 ﻿import { ISendStandardMessageRequestBody } from "./ISendStandardMessageRequestBody.js";
 
+/**
+ * Represents the request body for sending any kind of message.
+ * @privateRemarks Currently, only standard messages are supported, but this type may be
+ *                 updated in the future to include other message types.
+ */
 export type ISendMessageRequestBody = ISendStandardMessageRequestBody;
-
