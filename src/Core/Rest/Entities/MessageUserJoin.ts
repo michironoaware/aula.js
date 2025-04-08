0152458effb2a36155dd57316bb4aa5b4201c835
Rex Aliis <rexaliisdev@gmail.com>
2025-04-08 00:45:42 -0300

    document MessageUserJoin

--- src/Core/Rest/Entities/MessageUserJoin.ts
+++ src/Core/Rest/Entities/MessageUserJoin.ts
@@ -3,11 +3,20 @@ import { MessageUserJoinData } from "./Models/MessageUserJoinData.js";
 import { ThrowHelper } from "../../../Common/ThrowHelper.js";
 import { RestClient } from "../RestClient.js";
 
+/**
+ * Holds the additional data included in {@link MessageType.UserJoin} messages.
+ * */
 export class MessageUserJoin
 {
 	readonly #_restClient: RestClient;
 	readonly #_data: MessageUserJoinData;
 
+	/**
+	 * Initializes a new instance of {@link MessageUserJoin}.
+	 * @param data A DTO containing the entity data.
+	 * @param restClient The {@link RestClient} that is initializing this instance.
+	 * @package
+	 * */
 	public constructor(data: MessageUserJoinData, restClient: RestClient)
 	{
 		SealedClassError.throwIfNotEqual(MessageUserJoin, new.target);
@@ -18,26 +27,43 @@ export class MessageUserJoin
 		this.#_data = data;
 	}
 
+	/**
+	 * Gets the {@link RestClient} that initialized this instance.
+	 * */
 	public get restClient()
 	{
 		return this.#_restClient;
 	}
 
+	/**
+	 * Gets the id of the user who joined the room.
+	 * */
 	public get userId()
 	{
 		return this.#_data.userId;
 	}
 
+	/**
+	 * Gets the id of the room where the user comes from.
+	 * */
 	public get previousRoomId()
 	{
 		return this.#_data.previousRoomId;
 	}
 
+	/**
+	 * Gets the user that joined the room.
+	 * @returns A promise that resolves to a {@link User}.
+	 * */
 	public async getUser()
 	{
 		return (await this.restClient.getUser(this.userId))!;
 	}
 
+	/**
+	 * Gets the room where the user comes from.
+	 * @returns A promise that resolves to a {@link Room}, or `null` if the room no longer exists.
+	 * */
 	public async getPreviousRoom()
 	{
 		return this.previousRoomId !== null ? await this.restClient.getRoom(this.previousRoomId) : null;
