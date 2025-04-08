1ccd63195d425d35a84a8ec4cf2616577a4a9a15
Rex Aliis <rexaliisdev@gmail.com>
2025-04-08 00:17:55 -0300

    document MessageFlags

--- src/Core/Rest/Entities/MessageFlags.ts
+++ src/Core/Rest/Entities/MessageFlags.ts
@@ -1,5 +1,11 @@
-﻿export const MessageFlags =
+﻿/**
+ * Enumerates the flags that can be applied to a message.
+ */
+export const MessageFlags =
 {
+	/**
+	 * Indicates that the author of the message should be hidden in the UI.
+	 */
 	HideAuthor: 1n,
 } as const;
 
