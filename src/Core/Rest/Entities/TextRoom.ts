acb042a6aeee04c0c8e8a9a2a0b5acff6ede03ee
Rex Aliis <rexaliisdev@gmail.com>
2025-04-08 19:11:42 -0300

    document TextRoom

--- src/Core/Rest/Entities/TextRoom.ts
+++ src/Core/Rest/Entities/TextRoom.ts
@@ -11,8 +11,18 @@ import { MessageType } from "./MessageType.js";
 import { RoomType } from "./RoomType.js";
 import { InvalidOperationError } from "../../../Common/InvalidOperationError.js";
 
+/**
+ * Represents a text room within Aula.
+ * A text room allows communication between users through text messages.
+ * */
 export class TextRoom extends Room
 {
+	/**
+	 * Initializes a new instance of {@link TextRoom}.
+	 * @param data A DTO containing the entity data.
+	 * @param restClient The {@link RestClient} that is initializing this instance.
+	 * @package
+	 * */
 	public constructor(data: RoomData, restClient: RestClient)
 	{
 		super(data, restClient);
@@ -24,28 +34,52 @@ export class TextRoom extends Room
 		}
 	}
 
+	/**
+	 * Notifies that the user started typing in the room.
+	 * @returns A promise that resolves once the operation is complete.
+	 * */
 	public async startTyping()
 	{
 		return await this.restClient.startTyping(this.id);
 	}
 
+	/**
+	 * Notifies that the user stopped typing in the room.
+	 * @returns A promise that resolves once the operation is complete.
+	 * */
 	public async stopTyping()
 	{
 		return await this.restClient.stopTyping(this.id);
 	}
 
+	/**
+	 * Gets a message sent in the room.
+	 * @param messageId the id of the message to retrieve.
+	 * @returns A promise that resolves to a {@link Message},
+	 * or `null` if the message does not exist.
+	 * */
 	public async getMessage(messageId: string)
 	{
 		ThrowHelper.TypeError.throwIfNotType(messageId, "string");
 		return await this.restClient.getMessage(this.id, messageId);
 	}
 
+	/**
+	 * Gets a collection of messages sent in the room.
+	 * @param query The parameters for retrieving messages.
+	 * @returns A promise that resolves to a {@link Message} array.
+	 * */
 	public async getMessages(query: IGetMessagesQuery = {})
 	{
 		ThrowHelper.TypeError.throwIfNullable(query);
 		return await this.restClient.getMessages(this.id, query);
 	}
 
+	/**
+	 * Sends a message in the room.
+	 * @param message The request body for sending the message.
+	 * @returns A promise that resolves to a {@link Message} representing the message sent.
+	 * */
 	public async sendMessage(message: ISendMessageRequestBody | string)
 	{
 		ThrowHelper.TypeError.throwIfNotAnyType(message, "object", "string");
