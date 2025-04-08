68c08ca264706666d0c21dbaaac012c0f04992f7
Rex Aliis <rexaliisdev@gmail.com>
2025-04-08 19:55:21 -0300

    Create JsonReplacer.ts

---
+++ src/Common/Json/JsonReplacer.ts
@@ -0,0 +1,7 @@
+﻿import { NullJsonReplacer } from "./NullJsonReplacer.js";
+import { BigIntJsonReplacer } from "./BigIntJsonReplacer.js";
+
+export const JsonReplacer = (key: unknown, value: unknown) =>
+{
+	return NullJsonReplacer(key, BigIntJsonReplacer(key, value));
+};
