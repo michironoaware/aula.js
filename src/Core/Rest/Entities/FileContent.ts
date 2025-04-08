2129415ae969a4eb9721407fd09a4612e64cf526
Rex Aliis <rexaliisdev@gmail.com>
2025-04-07 21:39:07 -0300

    document FileContent

--- src/Core/Rest/Entities/FileContent.ts
+++ src/Core/Rest/Entities/FileContent.ts
@@ -4,11 +4,19 @@ import { SealedClassError } from "../../../Common/SealedClassError.js";
 import { IDisposable } from "../../../Common/IDisposable.js";
 import { ObjectDisposedError } from "../../../Common/ObjectDisposedError.js";
 
+/**
+ * Represents the contents of a file hosted in Aula.
+ * */
 export class FileContent implements IDisposable
 {
 	readonly #_httpContent: HttpContent;
 	#_disposed: boolean = false;
 
+	/**
+	 * Initializes a new instance of {@link FileContent}.
+	 * @param httpContent The {@link HttpContent} holding the contents of the file.
+	 * @package
+	 * */
 	public constructor(httpContent: HttpContent)
 	{
 		SealedClassError.throwIfNotEqual(FileContent, new.target);
@@ -17,12 +25,22 @@ export class FileContent implements IDisposable
 		this.#_httpContent = httpContent;
 	}
 
+	/**
+	 * Reads the content as a stream.
+	 * @returns A promise that resolves to a {@link ReadableStream}.
+	 * @throws {ObjectDisposedError} If the instance has been disposed.
+	 */
 	public async readAsStream()
 	{
 		ObjectDisposedError.throwIf(this.#_disposed);
 		return await this.#_httpContent.readAsStream();
 	}
 
+	/**
+	 * Reads the content as a byte array.
+	 * @returns A promise that resolves to a {@link Uint8Array}.
+	 * @throws {ObjectDisposedError} If the instance has been disposed.
+	 * */
 	public async readAsByteArray()
 	{
 		ObjectDisposedError.throwIf(this.#_disposed);
