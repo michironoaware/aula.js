7a870ea80b764cd5efa2b05eae2fb5e89f873f6f
Rex Aliis <rexaliisdev@gmail.com>
2025-04-08 19:12:10 -0300

    document ISendUnknownMessageRequestBody

--- src/Core/Rest/ISendUnknownMessageRequestBody.ts
+++ src/Core/Rest/ISendUnknownMessageRequestBody.ts
@@ -1,9 +1,23 @@
 ﻿import { MessageType } from "./Entities/MessageType.js";
 import { MessageFlags } from "./Entities/MessageFlags.js";
 
+/**
+ * The payload structure for sending a message of an unknown type.
+ */
 export interface ISendUnknownMessageRequestBody
 {
+	/**
+	 * The type of the message being sent.
+	 */
 	readonly type: Exclude<MessageType, typeof MessageType.UserLeave | typeof MessageType.UserJoin>;
+
+	/**
+	 * The flags of the message being sent.
+	 */
 	readonly flags?: MessageFlags;
+
+	/**
+	 * The text content of the message being sent.
+	 */
 	readonly content?: string;
 }
