88051ca68e8f6c9475b8c8e97a936826b8f9ac62
Rex Aliis <rexaliisdev@gmail.com>
2025-04-08 16:25:55 -0300

    document Presence

--- src/Core/Rest/Entities/Presence.ts
+++ src/Core/Rest/Entities/Presence.ts
@@ -1,6 +1,18 @@
+/**
+ * Enumerates the presence statuses a user can have.
+ * */
 export const Presence =
 {
+	/**
+	 * The user is offline and not currently active.
+	 * This may also represent an invisible or disconnected state.
+	 */
 	Offline: 0,
+
+	/**
+	 * The user is online and actively connected.
+	 * Indicates availability and presence in the application.
+	 */
 	Online: 1,
 } as const;
 
