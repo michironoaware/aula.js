41387cc7640766e9aa3ac6548a9d70a7ef3c9bff
Rex Aliis <rexaliisdev@gmail.com>
2025-04-08 19:12:19 -0300

    document ISendStandardMessageRequestBody

--- src/Core/Rest/ISendStandardMessageRequestBody.ts
+++ src/Core/Rest/ISendStandardMessageRequestBody.ts
@@ -1,8 +1,18 @@
 ﻿import { ISendUnknownMessageRequestBody } from "./ISendUnknownMessageRequestBody.js";
 import { MessageType } from "./Entities/MessageType.js";
 
+/**
+ * The payload structure for sending a standard text message.
+ */
 export interface ISendStandardMessageRequestBody extends ISendUnknownMessageRequestBody
 {
+	/**
+	 * The type of the message. Must be {@link MessageType.Standard}.
+	 */
 	readonly type: typeof MessageType.Standard;
+
+	/**
+	 * The message content. This field is required for standard messages.
+	 */
 	readonly content: string;
 }
