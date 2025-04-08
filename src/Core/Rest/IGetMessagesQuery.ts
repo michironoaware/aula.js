074da333f7f55ad3d9af6e4302f5b6a3a96a2d7e
Rex Aliis <rexaliisdev@gmail.com>
2025-04-08 19:12:35 -0300

    document IGetMessagesQuery

--- src/Core/Rest/IGetMessagesQuery.ts
+++ src/Core/Rest/IGetMessagesQuery.ts
@@ -1,6 +1,23 @@
-﻿export interface IGetMessagesQuery
+﻿/**
+ * Query parameters for retrieving messages.
+ */
+export interface IGetMessagesQuery
 {
+	/**
+	 * An optional message id to retrieve messages sent **before** this message.
+	 * Useful for paginating backwards in a message list.
+	 */
 	readonly before?: string;
+
+	/**
+	 * An optional message id to retrieve messages sent **after** this message.
+	 * Useful for paginating forwards in a message list.
+	 */
 	readonly after?: string;
+
+	/**
+	 * The maximum number of messages to retrieve.
+	 * If not specified, a default server-defined limit will be applied.
+	 */
 	readonly count?: number;
 }
