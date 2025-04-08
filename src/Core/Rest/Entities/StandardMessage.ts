4f6eb68783212e7ccca3826e40d3d7908f3ab47e
Rex Aliis <rexaliisdev@gmail.com>
2025-04-08 17:33:53 -0300

    document StandardMessage

--- src/Core/Rest/Entities/StandardMessage.ts
+++ src/Core/Rest/Entities/StandardMessage.ts
@@ -5,10 +5,19 @@ import { SealedClassError } from "../../../Common/SealedClassError.js";
 import { MessageType } from "./MessageType.js";
 import { InvalidOperationError } from "../../../Common/InvalidOperationError.js";
 
+/**
+ * Represents a text message within Aula.
+ * */
 export class StandardMessage extends Message
 {
 	readonly #_data: MessageData;
 
+	/**
+	 * Initializes a new instance of {@link StandardMessage}.
+	 * @param data A DTO containing the entity data.
+	 * @param restClient The {@link RestClient} that is initializing this instance.
+	 * @package
+	 * */
 	public constructor(data: MessageData, restClient: RestClient)
 	{
 		super(data, restClient);
@@ -22,11 +31,18 @@ export class StandardMessage extends Message
 		this.#_data = data;
 	}
 
+	/**
+	 * Gets the text content of the message.
+	 * */
 	public get text()
 	{
 		return this.#_data.text!;
 	}
 
+	/**
+	 * Gets the latest version of the message.
+	 * @returns A promise that resolves to a {@link StandardMessage}, or `null` if the message no longer exists.
+	 * */
 	public async getLatest()
 	{
 		return await super.getLatest() as StandardMessage | null;
