1c936ac89a8e42c9e463d58f800b16fbc6e03314
Rex Aliis <rexaliisdev@gmail.com>
2025-04-08 21:09:35 -0300

    document AulaForbiddenError

--- src/Core/Rest/AulaForbiddenError.ts
+++ src/Core/Rest/AulaForbiddenError.ts
@@ -3,8 +3,17 @@ import { SealedClassError } from "../../Common/SealedClassError.js";
 import { HttpRequestError } from "../../Common/Http/HttpRequestError.js";
 import { ProblemDetails } from "./Entities/Models/ProblemDetails.js";
 
+/**
+ * The error that occurs when an operation could not be completed because there was no permission to access the resource.
+ * */
 export class AulaForbiddenError extends AulaRestError
 {
+	/**
+	 * Initializes a new instance of {@link AulaForbiddenError}.
+	 * @param problemDetails The problem details of the request error.
+	 * @param innerError the {@link HttpRequestError} related to the error.
+	 * @package
+	 * */
 	public constructor(
 		problemDetails: ProblemDetails,
 		innerError?: HttpRequestError)
