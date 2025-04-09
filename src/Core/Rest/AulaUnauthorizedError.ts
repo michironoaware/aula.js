3bdf83e9daff92ab77f4e3a940c7b078872453d3
Rex Aliis <rexaliisdev@gmail.com>
2025-04-08 21:24:58 -0300

    document AulaUnauthorizedError

--- src/Core/Rest/AulaUnauthorizedError.ts
+++ src/Core/Rest/AulaUnauthorizedError.ts
@@ -3,8 +3,17 @@ import { SealedClassError } from "../../Common/SealedClassError.js";
 import { HttpRequestError } from "../../Common/Http/HttpRequestError.js";
 import { ProblemDetails } from "./Entities/Models/ProblemDetails.js";
 
+/**
+ * The error that occurs when the provided authorization credentials are missing or invalid.
+ * */
 export class AulaUnauthorizedError extends AulaRestError
 {
+	/**
+	 * Initializes a new instance of {@link AulaUnauthorizedError}.
+	 * @param problemDetails The problem details of the request error.
+	 * @param innerError the {@link HttpRequestError} related to the error.
+	 * @package
+	 * */
 	public constructor(
 		problemDetails: ProblemDetails,
 		innerError?: HttpRequestError)
