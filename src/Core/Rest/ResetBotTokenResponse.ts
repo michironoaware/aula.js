fd36e7043a15a85331ffca1ef9aa5360f85fd28d
Rex Aliis <rexaliisdev@gmail.com>
2025-04-08 21:55:37 -0300

    add restClient property

--- src/Core/Rest/ResetBotTokenResponse.ts
+++ src/Core/Rest/ResetBotTokenResponse.ts
@@ -1,5 +1,6 @@
 ﻿import { ThrowHelper } from "../../Common/ThrowHelper.js";
 import { SealedClassError } from "../../Common/SealedClassError.js";
+import { RestClient } from "./RestClient.js";
 
 /**
  * Represents the result of a successful bot token reset operation.
@@ -7,20 +8,32 @@ import { SealedClassError } from "../../Common/SealedClassError.js";
 export class ResetBotTokenResponse
 {
 	readonly #_token: string;
+	readonly #_restClient: RestClient;
 
 	/**
 	 * Initializes a new instance of {@link ResetBotTokenResponse}.
 	 * @param data An object that conforms to the API v1 ResetBotTokenResponseBody JSON schema
 	 *             from where the data will be extracted.
+	 * @param restClient The {@link RestClient} that is initializing this instance.
 	 * @package
 	 * */
-	public constructor(data: any)
+	public constructor(data: any, restClient: RestClient)
 	{
 		SealedClassError.throwIfNotEqual(ResetBotTokenResponse, new.target);
 		ThrowHelper.TypeError.throwIfNullable(data);
 		ThrowHelper.TypeError.throwIfNotType(data.token, "string");
+		ThrowHelper.TypeError.throwIfNotType(restClient, RestClient);
 
 		this.#_token = data.token;
+		this.#_restClient = restClient;
+	}
+
+	/**
+	 * Gets the {@link RestClient} that initialized this instance.
+	 * */
+	public get restClient()
+	{
+		return this.#_restClient;
 	}
 
 	/**
