6dd71f21fd6fff313b28dd49e046490005e40c5a
Rex Aliis <rexaliisdev@gmail.com>
2025-04-08 21:09:43 -0300

    document AulaNotFoundError

--- src/Core/Rest/AulaNotFoundError.ts
+++ src/Core/Rest/AulaNotFoundError.ts
@@ -3,8 +3,17 @@ import { SealedClassError } from "../../Common/SealedClassError.js";
 import { HttpRequestError } from "../../Common/Http/HttpRequestError.js";
 import { ProblemDetails } from "./Entities/Models/ProblemDetails.js";
 
+/**
+ * The error that occurs when a requested resource does not exist.
+ * */
 export class AulaNotFoundError extends AulaRestError
 {
+	/**
+	 * Initializes a new instance of {@link AulaNotFoundError}.
+	 * @param problemDetails The problem details of the request error.
+	 * @param innerError the {@link HttpRequestError} related to the error.
+	 * @package
+	 * */
 	public constructor(
 		problemDetails: ProblemDetails,
 		innerError?: HttpRequestError)
