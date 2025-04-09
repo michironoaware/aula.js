7fb6af4b3105dc357ea1d056c1d92057b86763bc
Rex Aliis <rexaliisdev@gmail.com>
2025-04-08 21:54:38 -0300

    document ResetBotTokenResponse

--- src/Core/Rest/ResetBotTokenResponse.ts
+++ src/Core/Rest/ResetBotTokenResponse.ts
@@ -1,10 +1,19 @@
 ﻿import { ThrowHelper } from "../../Common/ThrowHelper.js";
 import { SealedClassError } from "../../Common/SealedClassError.js";
 
+/**
+ * Represents the result of a successful bot token reset operation.
+ * */
 export class ResetBotTokenResponse
 {
 	readonly #_token: string;
 
+	/**
+	 * Initializes a new instance of {@link ResetBotTokenResponse}.
+	 * @param data An object that conforms to the API v1 ResetBotTokenResponseBody JSON schema
+	 *             from where the data will be extracted.
+	 * @package
+	 * */
 	public constructor(data: any)
 	{
 		SealedClassError.throwIfNotEqual(ResetBotTokenResponse, new.target);
@@ -14,6 +23,9 @@ export class ResetBotTokenResponse
 		this.#_token = data.token;
 	}
 
+	/**
+	 * Gets the new authorization token of the bot.
+	 * */
 	public get token()
 	{
 		return this.#_token;
