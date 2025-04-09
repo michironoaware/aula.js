d03f74e50aec0e082baf1bc02039ea47f65faa7a
Rex Aliis <rexaliisdev@gmail.com>
2025-04-08 21:39:06 -0300

    add restClient property

--- src/Core/Rest/GetCurrentUserBanStatusResponse.ts
+++ src/Core/Rest/GetCurrentUserBanStatusResponse.ts
@@ -1,5 +1,6 @@
 ﻿import { ThrowHelper } from "../../Common/ThrowHelper.js";
 import { SealedClassError } from "../../Common/SealedClassError.js";
+import { RestClient } from "./RestClient.js";
 
 /**
  * Holds information about the current user's ban status.
@@ -7,20 +8,32 @@ import { SealedClassError } from "../../Common/SealedClassError.js";
 export class GetCurrentUserBanStatusResponse
 {
 	readonly #_banned: boolean;
+	readonly #_restClient: RestClient;
 
 	/**
 	 * Initializes a new instance of {@link GetCurrentUserBanStatusResponse}.
 	 * @param data An object that conforms to the API v1 GetCurrentUserBanStatusResponseBody JSON schema
 	 *             from where the data will be extracted.
+	 * @param restClient The {@link RestClient} that is initializing this instance.
 	 * @package
 	 * */
-	constructor(data: any)
+	constructor(data: any, restClient: RestClient)
 	{
 		SealedClassError.throwIfNotEqual(GetCurrentUserBanStatusResponse, new.target);
 		ThrowHelper.TypeError.throwIfNullable(data);
 		ThrowHelper.TypeError.throwIfNotType(data.banned, "boolean");
+		ThrowHelper.TypeError.throwIfNotType(restClient, RestClient);
 
 		this.#_banned = data.banned;
+		this.#_restClient = restClient;
+	}
+
+	/**
+	 * Gets the {@link RestClient} that initialized this instance.
+	 * */
+	public get restClient()
+	{
+		return this.#_restClient;
 	}
 
 	/**
