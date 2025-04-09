49d3e886452722e9acb6c762f37aa5e91c6fc95e
Rex Aliis <rexaliisdev@gmail.com>
2025-04-08 21:37:27 -0300

    document GetCurrentUserBanStatusResponse

--- src/Core/Rest/GetCurrentUserBanStatusResponse.ts
+++ src/Core/Rest/GetCurrentUserBanStatusResponse.ts
@@ -1,10 +1,19 @@
 ﻿import { ThrowHelper } from "../../Common/ThrowHelper.js";
 import { SealedClassError } from "../../Common/SealedClassError.js";
 
+/**
+ * Holds information about the current user's ban status.
+ * */
 export class GetCurrentUserBanStatusResponse
 {
 	readonly #_banned: boolean;
 
+	/**
+	 * Initializes a new instance of {@link GetCurrentUserBanStatusResponse}.
+	 * @param data An object that conforms to the API v1 GetCurrentUserBanStatusResponseBody JSON schema
+	 *             from where the data will be extracted.
+	 * @package
+	 * */
 	constructor(data: any)
 	{
 		SealedClassError.throwIfNotEqual(GetCurrentUserBanStatusResponse, new.target);
@@ -14,6 +23,9 @@ export class GetCurrentUserBanStatusResponse
 		this.#_banned = data.banned;
 	}
 
+	/**
+	 * Gets whether the current user is banned from the application.
+	 * */
 	public get banned()
 	{
 		return this.#_banned;
