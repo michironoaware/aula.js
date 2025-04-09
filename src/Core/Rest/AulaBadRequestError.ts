26d4b4898b774271705133469cdcff57afbc71ab
Rex Aliis <rexaliisdev@gmail.com>
2025-04-08 21:09:27 -0300

    document AulaBadRequestError

--- src/Core/Rest/AulaBadRequestError.ts
+++ src/Core/Rest/AulaBadRequestError.ts
@@ -3,8 +3,17 @@ import { SealedClassError } from "../../Common/SealedClassError.js";
 import { HttpRequestError } from "../../Common/Http/HttpRequestError.js";
 import { ProblemDetails } from "./Entities/Models/ProblemDetails.js";
 
+/**
+ * The error that occurs when a request was improperly formatted, or the server couldn't understand it.
+ * */
 export class AulaBadRequestError extends AulaRestError
 {
+	/**
+	 * Initializes a new instance of {@link AulaBadRequestError}.
+	 * @param problemDetails The problem details of the request error.
+	 * @param innerError the {@link HttpRequestError} related to the error.
+	 * @package
+	 * */
 	public constructor(
 		problemDetails: ProblemDetails,
 		innerError?: HttpRequestError)
