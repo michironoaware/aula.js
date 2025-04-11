b370f78dff144d21c988c14cd8919bc6badf630d
rexaliis <77251557+rexaliis@users.noreply.github.com>
2025-04-11 13:02:07 -0300

    Delete IModifyRoomRequestBody.ts

--- src/Core/Rest/IModifyRoomRequestBody.ts
+++
@@ -1,7 +0,0 @@
-﻿export interface IModifyRoomRequestBody
-{
-	readonly name?: string | null;
-	readonly description?: string | null;
-	readonly isEntrance?: boolean | null;
-	readonly backgroundAudioId?: string | null;
-}
