dcaed46ba81e810fee9e0b85a19db7f5a67551fa
rexaliis <77251557+rexaliis@users.noreply.github.com>
2025-04-11 11:22:52 -0300

    Delete IGetUsersQuery.ts

--- src/Core/Rest/IGetUsersQuery.ts
+++
@@ -1,8 +0,0 @@
-﻿import { UserType } from "./Entities/UserType.js";
-
-export interface IGetUsersQuery
-{
-	readonly type?: UserType | null;
-	readonly count?: number | null;
-	readonly after?: string | null;
-}
