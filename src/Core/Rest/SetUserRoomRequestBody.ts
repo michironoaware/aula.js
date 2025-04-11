2bfa61bc9d9b156c4af35e2f7632d59623297451
rexaliis <77251557+rexaliis@users.noreply.github.com>
2025-04-11 12:11:39 -0300

    Create SetUserRoomRequestBody.ts

---
+++ src/Core/Rest/SetUserRoomRequestBody.ts
@@ -0,0 +1,53 @@
+﻿import { SealedClassError } from "../../Common/SealedClassError.js";
+import { ThrowHelper } from "../../Common/ThrowHelper.js";
+
+/**
+ * Represents the request body used to set the user's room.
+ * @sealed
+ * */
+export class SetUserRoomRequestBody
+{
+	#_roomId: string | null = null;
+
+	/**
+	 * Initializes a new instance of {@link SetUserRoomRequestBody}.
+	 * */
+	public constructor()
+	{
+		SealedClassError.throwIfNotEqual(SetUserRoomRequestBody, new.target);
+	}
+
+	/**
+	 * Gets the id of the room to which the user will be relocated.
+	 * */
+	public get roomId()
+	{
+		return this.#_roomId;
+	}
+
+	/**
+	 * Sets the id of the room to which the user will be relocated.
+	 * @param roomId the id of the room, or `null` to relocate the user to no room.
+	 * */
+	public set roomId(roomId: string | null)
+	{
+		ThrowHelper.TypeError.throwIfNotAnyType(roomId, "string", "null");
+		this.#_roomId = roomId;
+	}
+
+	/**
+	 * Sets the id of the room to which the user will be relocated.
+	 * @param roomId the id of the room, or `null` to relocate the user to no room.
+	 * @return The current {@link SetUserRoomRequestBody} instance.
+	 * */
+	public withRoomId(roomId: string | null)
+	{
+		this.roomId = roomId;
+		return this;
+	}
+
+	public toJSON()
+	{
+		return { roomId: this.#_roomId };
+	}
+}
