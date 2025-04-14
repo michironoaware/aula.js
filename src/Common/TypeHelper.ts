e31a97aa154f1e6eb3c8c018ad7e8b7db7aba517
rexaliis <77251557+rexaliis@users.noreply.github.com>
2025-04-14 00:38:49 -0300

    optimize TypeHelper.isType method

--- src/Common/TypeHelper.ts
+++ src/Common/TypeHelper.ts
@@ -7,28 +7,33 @@
 
 	export function isType<T extends TypeResolvable>(object: unknown, type: T): object is ResolvedType<T>
 	{
-		return (
-			// Check whether typeof of the object is the same as the type string.
-			(typeof type === "string" && typeof object === type) ||
+		if (typeof type === "string")
+		{
+			return (
+				// Check whether typeof of the object is the same as the type string.
+				typeof object === type ||
 
+				// Check if object is null for type "null"
+				(type === "null" && object === null) ||
+
+				// Check if object is either null or undefined for type "nullable"
+				(type === "nullable" && isNullable(object)) ||
+
+				// Check if object is iterable for type "iterable"
+				(type === "iterable" && (object as any)[Symbol.iterator] !== undefined) ||
+
+				// Check if object is array for type "array"
+				(type === "array" && Array.isArray(object))
+			);
+		}
+
+		return (
 			// Check if the object is an instance of the specified class
 			(typeof type === "function" && object instanceof type) ||
 
-			// Check if object is null for type "null"
-			(type === "null" && object === null) ||
-
-			// Check if object is either null or undefined for type "nullable"
-			(type === "nullable" && isNullable(object)) ||
-
 			// For checking whether a numeric value is defined in the enum's members
 			// does not work with flag enums
-			(typeof type === "object" && type[object as any] !== undefined) ||
-
-			// Check if object is iterable for type "iterable"
-			(type === "iterable" && (object as any)[Symbol.iterator] !== undefined) ||
-
-			// Check if object is array for type "array"
-			(type === "array" && Array.isArray(object))
+			(typeof type === "object" && type[object as any] !== undefined)
 		);
 	}
 
