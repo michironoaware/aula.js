42ba5d5fac1fef95176f0a4aebd0da7aa8dec486
rexaliis <77251557+rexaliis@users.noreply.github.com>
2025-04-14 23:26:17 -0300

    Create SetUserPermissionsRequestBody.ts

---
+++ src/Core/Rest/SetUserPermissionsRequestBody.ts
@@ -0,0 +1,49 @@
+﻿import { SealedClassError } from "../../Common/SealedClassError.js";
+import { ThrowHelper } from "../../Common/ThrowHelper.js";
+import { Permissions } from "./Entities/Permissions.js";
+
+/**
+ * Represents the request body used to override a user's permissions.
+ * @sealed
+ */
+export class SetUserPermissionsRequestBody
+{
+	#_permissions: Permissions | null = null;
+
+	/**
+	 * Initializes a new instance of {@link SetUserPermissionsRequestBody}.
+	 * */
+	public constructor()
+	{
+		SealedClassError.throwIfNotEqual(SetUserPermissionsRequestBody, new.target);
+	}
+
+	/**
+	 * Gets the new permissions for the user.
+	 * */
+	public get permissions()
+	{
+		return this.#_permissions;
+	}
+
+	/**
+	 * Sets the new permissions for the user.
+	 * @param permissions The permission bit flags.
+	 * */
+	public set permissions(permissions: Permissions | null)
+	{
+		ThrowHelper.TypeError.throwIfNotAnyType(permissions, "number", "null");
+		this.#_permissions = permissions;
+	}
+
+	/**
+	 * Sets the new permissions for the user.
+	 * @param permissions The permission bit flags.
+	 * @returns The current {@link SetUserPermissionsRequestBody} instance. 
+	 * */
+	public withPermissions(permissions: Permissions | null)
+	{
+		this.permissions = permissions;
+		return this;
+	}
+}
