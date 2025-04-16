2527cc25748bbbe5095caac1b1ef05d2cffbbd0e
rexaliis <77251557+rexaliis@users.noreply.github.com>
2025-04-15 22:47:41 -0300

    document NotImplementedError

--- src/Common/NotImplementedError.ts
+++ src/Common/NotImplementedError.ts
@@ -1,10 +1,15 @@
 ﻿import { SealedClassError } from "./SealedClassError.js";
 
 /**
+ * The exception that is thrown when a requested method or operation is not implemented.
  * @sealed
+ * @package
  * */
 export class NotImplementedError extends Error
 {
+	/**
+	 * Initializes a new instance of {@link NotImplementedError}.
+	 * */
 	public constructor()
 	{
 		super("Not implemented");
