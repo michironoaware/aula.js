66e167100247cf6968a12fa86f8d2a7dab294b6d
rexaliis <77251557+rexaliis@users.noreply.github.com>
2025-04-15 22:51:44 -0300

    document ObjectDisposedError

--- src/Common/ObjectDisposedError.ts
+++ src/Common/ObjectDisposedError.ts
@@ -3,16 +3,24 @@ import { SealedClassError } from "./SealedClassError.js";
 import { InvalidOperationError } from "./InvalidOperationError.js";
 
 /**
+ * The exception that is thrown when an operation is performed on a disposed object.
  * @sealed
  * */
 export class ObjectDisposedError extends InvalidOperationError
 {
+	/**
+	 * Initializes a new instance of {@link ObjectDisposedError}.
+	 * */
 	public constructor()
 	{
 		super("Cannot access a disposed object");
 		SealedClassError.throwIfNotEqual(ObjectDisposedError, new.target);
 	}
 
+	/**
+	 * Throws an {@link ObjectDisposedError} if the specified condition is true.
+	 * @param condition The condition to evaluate.
+	 * */
 	public static throwIf(condition: boolean): asserts condition is false
 	{
 		ThrowHelper.TypeError.throwIfNotType(condition, "boolean");
