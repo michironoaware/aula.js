f472b1ac322d41e75e04f6d812da230a90f33c2d
rexaliis <77251557+rexaliis@users.noreply.github.com>
2025-04-15 22:43:41 -0300

    document SemaphoreFullError

--- src/Common/Threading/SemaphoreFullError.ts
+++ src/Common/Threading/SemaphoreFullError.ts
@@ -1,10 +1,15 @@
 ﻿import { SealedClassError } from "../SealedClassError.js";
 
 /**
+ * The exception that is thrown when the {@link Semaphore.release}
+ * method is called on a semaphore whose count is already at the maximum.
  * @sealed
  * */
 export class SemaphoreFullError extends Error
 {
+	/**
+	 * Initializes a new instance of {@link SemaphoreFullError}.
+	 * */
 	public constructor()
 	{
 		super("The semaphore count is already at the maximum value");
