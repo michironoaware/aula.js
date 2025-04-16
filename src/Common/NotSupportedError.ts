e37c1a37277bc2e0ae4ea61c3b5e76981a13dff8
rexaliis <77251557+rexaliis@users.noreply.github.com>
2025-04-15 22:49:33 -0300

    document NotSupportedError

--- src/Common/NotSupportedError.ts
+++ src/Common/NotSupportedError.ts
@@ -2,10 +2,15 @@
 import { ThrowHelper } from "./ThrowHelper.js";
 
 /**
+ * The exception that is thrown when an invoked method is not supported.
  * @sealed
  * */
 export class NotSupportedError extends Error
 {
+	/**
+	 * Initializes a new instance of {@link NotSupportedError}.
+	 * @param message The error message.
+	 * */
 	public constructor(message: string = "The operation is not supported")
 	{
 		super(message);
