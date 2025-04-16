d2cabc87ed7fdbb174eeb90fc0f92a9d5ba277a8
rexaliis <77251557+rexaliis@users.noreply.github.com>
2025-04-15 22:42:38 -0300

    document OperationCanceledError

--- src/Common/Threading/OperationCanceledError.ts
+++ src/Common/Threading/OperationCanceledError.ts
@@ -1,10 +1,14 @@
 ﻿import { SealedClassError } from "../SealedClassError.js";
 
 /**
+ * The exception that is thrown in a thread upon cancellation of an operation that the thread was executing.
  * @sealed
  * */
 export class OperationCanceledError extends Error
 {
+	/**
+	 * Initializes a new instance of {@link OperationCanceledError}.
+	 * */
 	public constructor()
 	{
 		super("The operation was canceled.");
