ec01ef00c87696995050d4a1c6b9aba14fdb34b9
rexaliis <77251557+rexaliis@users.noreply.github.com>
2025-04-15 22:08:23 -0300

    Create ResetPasswordRequestBody.ts

---
+++ src/Core/Rest/ResetPasswordRequestBody.ts
@@ -0,0 +1,86 @@
+﻿import { SealedClassError } from "../../Common/SealedClassError.js";
+import { ThrowHelper } from "../../Common/ThrowHelper.js";
+import { InvalidOperationError } from "../../Common/InvalidOperationError.js";
+
+/**
+ * Represents the request body of a password reset request.
+ * @sealed
+ * */
+export class ResetPasswordRequestBody
+{
+	#_code: string | null = null;
+	#_newPassword: string | null = null;
+
+	/**
+	 * Initializes a new instance of {@link ResetPasswordRequestBody}.
+	 * */
+	public constructor()
+	{
+		SealedClassError.throwIfNotEqual(ResetPasswordRequestBody, new.target);
+	}
+
+	/**
+	 * Gets the code required to reset the password.
+	 * */
+	public get code()
+	{
+		return this.#_code;
+	}
+
+	/**
+	 * Sets the code required to reset the password.
+	 * @param code The code string.
+	 * */
+	public set code(code: string | null)
+	{
+		ThrowHelper.TypeError.throwIfNotAnyType(code, "string", "null");
+		this.#_code = code;
+	}
+
+	/**
+	 * Gets the new password for the user.
+	 * */
+	public get newPassword()
+	{
+		return this.#_newPassword;
+	}
+
+	/**
+	 * Sets the new password for the user.
+	 * @param newPassword The password string.
+	 * */
+	public set newPassword(newPassword: string | null)
+	{
+		ThrowHelper.TypeError.throwIfNotAnyType(newPassword, "string", "null");
+		this.#_newPassword = newPassword;
+	}
+
+	/**
+	 * Sets the code required to reset the password.
+	 * @param code The code string.
+	 * @returns The current {@link ResetPasswordRequestBody} instance.
+	 * */
+	public withCode(code: string | null)
+	{
+		this.code = code;
+		return this;
+	}
+
+	/**
+	 * Sets the new password for the user.
+	 * @param newPassword The password string.
+	 * @returns The current {@link ResetPasswordRequestBody} instance.
+	 * */
+	public withNewPassword(newPassword: string | null)
+	{
+		this.newPassword = newPassword;
+		return this;
+	}
+
+	public toJSON()
+	{
+		InvalidOperationError.throwIf(this.#_code === null, "The password reset code must be provided first.");
+
+		return { code: this.#_code, newPassword: this.#_newPassword };
+	}
+}
