10e922a82bac4a571c24e4bf9e67c5df42f9aeea
rexaliis <77251557+rexaliis@users.noreply.github.com>
2025-04-21 10:55:29 -0300

    document Intents

--- src/Core/Gateway/Intents.ts
+++ src/Core/Gateway/Intents.ts
@@ -1,8 +1,39 @@
-﻿export const Intents =
+﻿import { EventType } from "./Models/EventType.js";
+
+/**
+ * Enumerates the intent values that can be passed when establishing a gateway connection.
+ * Intents define which events are delivered to the client.
+ * */
+export const Intents =
 {
+	/**
+	 * - {@link EventType.UserUpdated}
+	 * - {@link EventType.UserCurrentRoomUpdated}
+	 * - {@link EventType.UserPresenceUpdated}
+	 * */
 	Users: 1n,
+
+	/**
+	 * - {@link EventType.RoomCreated}
+	 * - {@link EventType.RoomRemoved}
+	 * - {@link EventType.RoomUpdated}
+	 * - {@link EventType.RoomConnectionCreated}
+	 * - {@link EventType.RoomConnectionRemoved}
+	 * */
 	Rooms: 2n,
+
+	/**
+	 * - {@link EventType.MessageCreated}
+	 * - {@link EventType.MessageRemoved}
+	 * - {@link EventType.UserStartedTyping}
+	 * - {@link EventType.UserStoppedTyping}
+	 * */
 	Messages: 4n,
+
+	/**
+	 * - {@link EventType.BanCreated}
+	 * - {@link EventType.BanRemoved}
+	 * */
 	Moderation: 8n,
 } as const;
 
