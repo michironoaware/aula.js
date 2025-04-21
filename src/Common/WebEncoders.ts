785306589150cd4f350a04856e0e9a8671738cc7
rexaliis <77251557+rexaliis@users.noreply.github.com>
2025-04-21 00:52:18 -0300

    Create WebEncoders.ts

---
+++ src/Common/WebEncoders.ts
@@ -0,0 +1,10 @@
+﻿export namespace WebEncoders
+{
+	export function ToBase64UrlString(input: string)
+	{
+		return btoa(input)
+			.replace(/\+/g, "-")
+			.replace(/\//g, "_")
+			.replace(/=+$/, "");
+	}
+}
