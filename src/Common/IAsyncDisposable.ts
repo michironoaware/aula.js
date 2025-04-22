0127011471483571af9432f60eace8dc2ca868e8
rexaliis <77251557+rexaliis@users.noreply.github.com>
2025-04-22 01:48:31 -0300

    Create IAsyncDisposable.ts

---
+++ src/Common/IAsyncDisposable.ts
@@ -0,0 +1,10 @@
+﻿/**
+ * Provides a mechanism for releasing unmanaged resources asynchronously.
+ * */
+export interface IAsyncDisposable
+{
+	/**
+	 * Performs application-defined tasks associated with freeing, releasing, or resetting resources asynchronously.
+	 */
+	[Symbol.asyncDispose](): Promise<void>;
+}
