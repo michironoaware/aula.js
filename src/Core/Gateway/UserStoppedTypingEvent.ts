353796e10b87ce91bdea8f9f1e8c04e851f49de9
rexaliis <77251557+rexaliis@users.noreply.github.com>
2025-04-22 00:07:58 -0300

    document UserStoppedTypingEvent

--- src/Core/Gateway/UserStoppedTypingEvent.ts
+++ src/Core/Gateway/UserStoppedTypingEvent.ts
@@ -4,6 +4,7 @@ import { SealedClassError } from "../../Common/SealedClassError.js";
 import { GatewayClient } from "./GatewayClient.js";
 
 /**
+ * Emitted when a user stopped typing a message.
  * @sealed
  * */
 export class UserStoppedTypingEvent
@@ -11,6 +12,9 @@ export class UserStoppedTypingEvent
 	readonly #_data: UserTypingEventData;
 	readonly #_gatewayClient: GatewayClient;
 
+	/**
+	 * @package
+	 * */
 	public constructor(data: UserTypingEventData, gatewayClient: GatewayClient)
 	{
 		SealedClassError.throwIfNotEqual(UserStoppedTypingEvent, new.target);
@@ -21,16 +25,25 @@ export class UserStoppedTypingEvent
 		this.#_gatewayClient = gatewayClient;
 	}
 
+	/**
+	 * Gets the id of the user that stopped typing.
+	 * */
 	public get userId()
 	{
 		return this.#_data.userId;
 	}
 
+	/**
+	 * Gets the id of the room where the user is no longer typing.
+	 * */
 	public get roomId()
 	{
 		return this.#_data.roomId;
 	}
 
+	/**
+	 * Gets the {@link GatewayClient} that initialized this instance.
+	 * */
 	public get gatewayClient()
 	{
 		return this.#_gatewayClient;
