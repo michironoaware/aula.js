5036597cfbd96f4455292b611dacee1e32d75be4
rexaliis <77251557+rexaliis@users.noreply.github.com>
2025-04-22 00:07:07 -0300

    document UserStartedTypingEvent

--- src/Core/Gateway/UserStartedTypingEvent.ts
+++ src/Core/Gateway/UserStartedTypingEvent.ts
@@ -4,6 +4,7 @@ import { SealedClassError } from "../../Common/SealedClassError.js";
 import { GatewayClient } from "./GatewayClient.js";
 
 /**
+ * Emitted when a user started typing a message.
  * @sealed
  * */
 export class UserStartedTypingEvent
@@ -11,6 +12,9 @@ export class UserStartedTypingEvent
 	readonly #_data: UserTypingEventData;
 	readonly #_gatewayClient: GatewayClient;
 
+	/**
+	 * @package
+	 * */
 	public constructor(data: UserTypingEventData, gatewayClient: GatewayClient)
 	{
 		SealedClassError.throwIfNotEqual(UserStartedTypingEvent, new.target);
@@ -21,16 +25,25 @@ export class UserStartedTypingEvent
 		this.#_gatewayClient = gatewayClient;
 	}
 
+	/**
+	 * Gets the id of the user that started typing.
+	 * */
 	public get userId()
 	{
 		return this.#_data.userId;
 	}
 
+	/**
+	 * Gets the id of the room where the user is typing.
+	 * */
 	public get roomId()
 	{
 		return this.#_data.roomId;
 	}
 
+	/**
+	 * Gets the {@link GatewayClient} that initialized this instance.
+	 * */
 	public get gatewayClient()
 	{
 		return this.#_gatewayClient;
