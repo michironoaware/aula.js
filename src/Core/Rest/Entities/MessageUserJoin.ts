5971d6483adfd22b85b3ef1db9d8094cb7e00da5
rexaliis <77251557+rexaliis@users.noreply.github.com>
2025-04-22 11:21:23 -0300

    accept a cancellationToken in MessageUserJoin async methods

--- src/Core/Rest/Entities/MessageUserJoin.ts
+++ src/Core/Rest/Entities/MessageUserJoin.ts
@@ -3,6 +3,7 @@ import { MessageUserJoinData } from "./Models/MessageUserJoinData.js";
 import { ThrowHelper } from "../../../Common/ThrowHelper.js";
 import { RestClient } from "../RestClient.js";
 import { UnreachableError } from "../../../Common/UnreachableError.js";
+import { CancellationToken } from "../../../Common/Threading/index.js";
 
 /**
  * Holds the additional data included in {@link MessageType.UserJoin} messages.
@@ -55,11 +56,13 @@ export class MessageUserJoin
 
 	/**
 	 * Gets the user who joined the room.
+	 * @param cancellationToken A {@link CancellationToken} to listen to.
 	 * @returns A promise that resolves to a {@link User}.
+	 * @throws {OperationCanceledError} If the {@link cancellationToken} has been signaled.
 	 * */
-	public async getUser()
+	public async getUser(cancellationToken: CancellationToken = CancellationToken.none)
 	{
-		const user = await this.restClient.getUser(this.userId);
+		const user = await this.restClient.getUser(this.userId, cancellationToken);
 		if (user === null)
 		{
 			throw new UnreachableError("User expected to exist, but the server sent nothing.");
@@ -70,11 +73,13 @@ export class MessageUserJoin
 
 	/**
 	 * Gets the room where the user comes from.
+	 * @param cancellationToken A {@link CancellationToken} to listen to.
 	 * @returns A promise that resolves to a {@link Room},
 	 * or `null` if the user did not come from any room or the room no longer exists.
+	 * @throws {OperationCanceledError} If the {@link cancellationToken} has been signaled.
 	 * */
-	public async getPreviousRoom()
+	public async getPreviousRoom(cancellationToken: CancellationToken = CancellationToken.none)
 	{
-		return this.previousRoomId !== null ? await this.restClient.getRoom(this.previousRoomId) : null;
+		return this.previousRoomId !== null ? await this.restClient.getRoom(this.previousRoomId, cancellationToken) : null;
 	}
 }
