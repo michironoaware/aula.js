6094e561f9636dfd7da6920143ceedc1c42285af
rexaliis <77251557+rexaliis@users.noreply.github.com>
2025-04-22 11:22:05 -0300

    accept a cancellationToken in User async methods

--- src/Core/Rest/Entities/User.ts
+++ src/Core/Rest/Entities/User.ts
@@ -9,6 +9,7 @@ import { UnreachableError } from "../../../Common/UnreachableError.js";
 import { SetUserRoomRequestBody } from "../SetUserRoomRequestBody.js";
 import { SetUserPermissionsRequestBody } from "../SetUserPermissionsRequestBody.js";
 import { BanUserRequestBody } from "../BanUserRequestBody.js";
+import { CancellationToken } from "../../../Common/Threading/index.js";
 
 /**
  * Represents a user within Aula.
@@ -103,11 +104,13 @@ export class User
 
 	/**
 	 * Gets the latest version of the user.
+	 * @param cancellationToken A {@link CancellationToken} to listen to.
 	 * @returns A promise that resolves to a {@link User}.
+	 * @throws {OperationCanceledError} If the {@link cancellationToken} has been signaled.
 	 * */
-	public async getLatest()
+	public async getLatest(cancellationToken: CancellationToken = CancellationToken.none)
 	{
-		const user = await this.restClient.getUser(this.id);
+		const user = await this.restClient.getUser(this.id, cancellationToken);
 		if (user === null)
 		{
 			throw new UnreachableError("User expected to exist, but the server sent nothing.");
@@ -118,60 +121,71 @@ export class User
 
 	/**
 	 * Gets the room where the user currently resides in.
+	 * @param cancellationToken A {@link CancellationToken} to listen to.
 	 * @returns A promise that resolves to a {@link Room},
 	 * or `null` if the user is not in any room or the room no longer exists.
+	 * @throws {OperationCanceledError} If the {@link cancellationToken} has been signaled.
 	 * */
-	public async getCurrentRoom()
+	public async getCurrentRoom(cancellationToken: CancellationToken = CancellationToken.none)
 	{
 		if (this.currentRoomId === null)
 		{
 			return null;
 		}
 
-		return await this.restClient.getRoom(this.currentRoomId);
+		return await this.restClient.getRoom(this.currentRoomId, cancellationToken);
 	}
 
 	/**
 	 * Relocates the user to the specified room.
 	 * @param room The id of the room, or `null` if the user should not be relocated.
+	 * @param cancellationToken A {@link CancellationToken} to listen to.
 	 * @returns A promise that resolves once the operation is complete.
+	 * @throws {OperationCanceledError} If the {@link cancellationToken} has been signaled.
 	 * */
-	public async setCurrentRoom(room: Room | string | null)
+	public async setCurrentRoom(room: Room | string | null, cancellationToken: CancellationToken = CancellationToken.none)
 	{
 		ThrowHelper.TypeError.throwIfNotAnyType(room, Room, "string", "null");
 
 		const roomId = TypeHelper.isType(room, Room) ? room.id : room ?? null;
-		return await this.restClient.setUserRoom(this.id, new SetUserRoomRequestBody().withRoomId(roomId));
+		return await this.restClient.setUserRoom(this.id, new SetUserRoomRequestBody().withRoomId(roomId), cancellationToken);
 	}
 
 	/**
 	 * Overwrites the permissions of the user.
 	 * @param permissions The new permissions of the user.
+	 * @param cancellationToken A {@link CancellationToken} to listen to.
 	 * @returns A promise that resolves once the operation is complete.
+	 * @throws {OperationCanceledError} If the {@link cancellationToken} has been signaled.
 	 * */
-	public async setPermissions(permissions: Permissions)
+	public async setPermissions(permissions: Permissions, cancellationToken: CancellationToken = CancellationToken.none)
 	{
 		ThrowHelper.TypeError.throwIfNotType(permissions, "bigint");
-		return await this.restClient.setUserPermissions(this.id, new SetUserPermissionsRequestBody().withPermissions(permissions));
+		return await this.restClient.setUserPermissions(
+			this.id, new SetUserPermissionsRequestBody().withPermissions(permissions), cancellationToken);
 	}
 
 	/**
 	 * Bans the user from the application.
 	 * @param reason A text that provides context about why the action was performed.
+	 * @param cancellationToken A {@link CancellationToken} to listen to.
 	 * @returns A promise that resolves to a {@link UserBan}, or `null` if the user is already banned.
+	 * @throws {OperationCanceledError} If the {@link cancellationToken} has been signaled.
 	 * */
-	public async ban(reason?: string)
+	public async ban(reason?: string, cancellationToken: CancellationToken = CancellationToken.none)
 	{
 		ThrowHelper.TypeError.throwIfNotAnyType(reason, "string", "undefined");
-		return await this.restClient.banUser(this.id, reason ? new BanUserRequestBody().withReason(reason) : undefined);
+		return await this.restClient.banUser(this.id, reason ? new BanUserRequestBody().withReason(reason) : undefined, cancellationToken);
 	}
 
 	/**
 	 * Unbans the user from the application.
+	 * @param cancellationToken A {@link CancellationToken} to listen to.
 	 * @returns A promise that resolves once the operation is complete.
+	 * @throws {OperationCanceledError} If the {@link cancellationToken} has been signaled.
 	 * */
-	public async unban()
+	public async unban(cancellationToken: CancellationToken = CancellationToken.none)
 	{
-		return await this.restClient.unbanUser(this.id);
+		return await this.restClient.unbanUser(this.id, cancellationToken);
 	}
 }
