fd82b8c0991f983e6b576fbd77a3a4aa89556a77
rexaliis <77251557+rexaliis@users.noreply.github.com>
2025-04-22 00:01:58 -0300

    document UserCurrentRoomUpdatedEvent

--- src/Core/Gateway/UserCurrentRoomUpdatedEvent.ts
+++ src/Core/Gateway/UserCurrentRoomUpdatedEvent.ts
@@ -4,6 +4,7 @@ import { SealedClassError } from "../../Common/SealedClassError.js";
 import { GatewayClient } from "./GatewayClient.js";
 
 /**
+ * Emitted when a user moves from room.
  * @sealed
  * */
 export class UserCurrentRoomUpdatedEvent
@@ -11,6 +12,9 @@ export class UserCurrentRoomUpdatedEvent
 	readonly #_data: UserCurrentRoomUpdatedEventData;
 	readonly #_gatewayClient: GatewayClient;
 
+	/**
+	 * @package
+	 * */
 	public constructor(data: UserCurrentRoomUpdatedEventData, gatewayClient: GatewayClient)
 	{
 		SealedClassError.throwIfNotEqual(UserCurrentRoomUpdatedEvent, new.target);
@@ -21,21 +25,35 @@ export class UserCurrentRoomUpdatedEvent
 		this.#_gatewayClient = gatewayClient;
 	}
 
+	/**
+	 * Gets the id of the user associated to the event.
+	 * */
 	public get userId()
 	{
 		return this.#_data.userId;
 	}
 
+	/**
+	 * Gets the id of the room where the user comes from,
+	 * or `null` if the user did not come from any room.
+	 * */
 	public get previousRoomId()
 	{
 		return this.#_data.previousRoomId;
 	}
 
+	/**
+	 * Gets the id of the room where the user moved to,
+	 * or `null` if the user was not relocated.
+	 * */
 	public get currentRoomId()
 	{
 		return this.#_data.currentRoomId;
 	}
 
+	/**
+	 * Gets the {@link GatewayClient} that initialized this instance.
+	 * */
 	public get gatewayClient()
 	{
 		return this.#_gatewayClient;
