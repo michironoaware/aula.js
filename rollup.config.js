4e6692586f7f9d10b4ffec448cf2a6ade117dd3c
rexaliis <77251557+rexaliis@users.noreply.github.com>
2025-04-23 01:14:43 -0300

    Create rollup.config.js

---
+++ rollup.config.js
@@ -0,0 +1,43 @@
+﻿import * as Path from "node:path";
+import typescript from "@rollup/plugin-typescript";
+import {terser} from "rollup-plugin-terser";
+
+export default [
+		{
+			input: "src/aula.ts",
+			output: {
+				file: "dist/aula.js",
+				format: "es",
+				name: "Aula",
+				//dir: "dist",
+				//preserveModules: true,
+				//preserveModulesRoot: 'src',
+			},
+			treeshake: false,
+			plugins: [
+				typescript({
+					tsconfig: 'tsconfig.json',
+					compilerOptions: { outDir: Path.dirname("dist/tsout") },
+				})
+			]
+		},
+	{
+		input: "src/aula.ts",
+		output: {
+			file: "dist/min/aula.js",
+			format: "es",
+			name: "Aula",
+			//dir: "dist/min",
+			//preserveModules: true,
+			//preserveModulesRoot: 'src',
+		},
+		treeshake: false,
+		plugins: [
+			typescript({
+				tsconfig: 'tsconfig.json',
+				compilerOptions: {outDir: Path.dirname("dist/min/tsout")},
+			}),
+			terser()
+		]
+	}
+];
