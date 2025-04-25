d5652a005068cfee4e9f946d688bc7d2f18816cd
rexaliis <77251557+michironoaware@users.noreply.github.com>
2025-04-25 06:26:04 -0300

    Create RestClientNullAddressError.ts

---
+++ src/Core/Rest/RestClientNullAddressError.ts
@@ -0,0 +1,23 @@
+﻿import { InvalidOperationError } from "../../Common/InvalidOperationError";
+import { SealedClassError } from "../../Common/SealedClassError";
+import { RestClient } from "./RestClient";
+
+/**
+ * Thrown when a {@link RestClient} method that does REST-related operations is called without the server address being defined first.
+ * */
+export class RestClientNullAddressError extends InvalidOperationError
+{
+	readonly #_restClient: RestClient;
+
+	/**
+	 * Initializes a new instance of {@link RestClientNullAddressError}.
+	 * */
+	constructor(restClient: RestClient)
+	{
+		super("No server address for the client has been defined.");
+		SealedClassError.throwIfNotEqual(RestClientNullAddressError, new.target);
+		//ThrowHelper.TypeError.throwIfNotType(restClient, RestClient); // Circular dependency problem
+
+		this.#_restClient = restClient;
+	}
+}
