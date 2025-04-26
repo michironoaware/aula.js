3c0aa14fbd43981832482507cfdc12b954e015f4
rexaliis <77251557+michironoaware@users.noreply.github.com>
2025-04-26 05:23:17 -0300

    add "Action" message flag

--- src/Core/Rest/Entities/MessageFlags.ts
+++ src/Core/Rest/Entities/MessageFlags.ts
@@ -7,6 +7,13 @@ export const MessageFlags =
 		 * Indicates that the author of the message should be hidden in the UI.
 		 */
 		HideAuthor: 1n,
+
+		/**
+		 * Indicates that the message content describes an action performed by the author.
+		 * When this flag is set, the message should be displayed with the author's display name
+		 * preceding the action text, to clearly attribute the action to the author.
+		 */
+		Action: 2n,
 	} as const;
 
 Object.freeze(MessageFlags);
