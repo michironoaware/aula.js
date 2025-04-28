352a184b5f6659849804b39a6a6e455e64fba7c4
rexaliis <77251557+michironoaware@users.noreply.github.com>
2025-04-28 10:46:46 -0300

    document LruCache

--- src/Common/Collections/LruCache.ts
+++ src/Common/Collections/LruCache.ts
@@ -6,8 +6,15 @@ import { UnreachableError } from "../UnreachableError";
 import { SealedClassError } from "../SealedClassError";
 
 /**
+ * Represents a Least-Recently-Used (LRU) cache that stores key-value pairs with a fixed maximum capacity.
+ *
+ * When the cache exceeds its defined capacity, the least recently accessed entry is automatically evicted.
+ *
+ * @template TKey The type of keys maintained by the cache. Keys must be non-nullable objects.
+ * @template TValue The type of mapped values. Values can be objects or `null`.
+ *
  * @sealed
- * */
+ */
 export class LruCache<TKey extends {}, TValue extends {} | null> implements Map<TKey, TValue>
 {
 	readonly #_order = new LinkedList<TKey>();
@@ -15,6 +22,11 @@ export class LruCache<TKey extends {}, TValue extends {} | null> implements Map<
 	#_size: number;
 	#_ignoreNewEntries: boolean;
 
+	/**
+	 * Initializes a new instance of the {@link LruCache} class with the specified capacity.
+	 * @param size The maximum number of entries the cache can hold.
+	 * @param ignoreNewEntries If set to `true`, new entries will be ignored when the cache is full. Defaults to `false`.
+	 */
 	public constructor(size: number, ignoreNewEntries: boolean = false)
 	{
 		SealedClassError.throwIfNotEqual(LruCache, new.target);
@@ -25,16 +37,25 @@ export class LruCache<TKey extends {}, TValue extends {} | null> implements Map<
 		this.#_ignoreNewEntries = ignoreNewEntries;
 	}
 
+	/**
+	 * Gets the current maximum capacity of the cache.
+	 */
 	public get size()
 	{
 		return this.#_size;
 	}
 
+	/**
+	 * Gets whether new entries should be ignored.
+	 */
 	public get ignoreNewEntries()
 	{
 		return this.#_ignoreNewEntries;
 	}
 
+	/**
+	 * Sets whether new entries should be ignored.
+	 */
 	public set ignoreNewEntries(value: boolean)
 	{
 		ThrowHelper.TypeError.throwIfNotType(value, "boolean");
@@ -46,6 +67,11 @@ export class LruCache<TKey extends {}, TValue extends {} | null> implements Map<
 		return "LruCache";
 	}
 
+	/**
+	 * Expands the cache's maximum capacity by a specified amount.
+	 * @param extraCapacity The number of additional entries the cache should be able to store.
+	 * @throws {ValueOutOfRangeError} If `extraCapacity` is negative.
+	 */
 	public expandSize(extraCapacity: number)
 	{
 		ThrowHelper.TypeError.throwIfNotType(extraCapacity, "number");
@@ -53,6 +79,11 @@ export class LruCache<TKey extends {}, TValue extends {} | null> implements Map<
 		this.#_size += extraCapacity;
 	}
 
+	/**
+	 * Retrieves the value associated with the specified key, and marks it as most recently used.
+	 * @param key The key to retrieve.
+	 * @returns The value associated with the key, or `undefined` if not found.
+	 */
 	public get(key: TKey)
 	{
 		ThrowHelper.TypeError.throwIfNullable(key);
@@ -72,17 +103,30 @@ export class LruCache<TKey extends {}, TValue extends {} | null> implements Map<
 		return entry.value;
 	}
 
+	/**
+	 * Determines whether the cache contains the specified key.
+	 * @param key The key to locate.
+	 * @returns `true` if the cache contains the key; otherwise, `false`.
+	 */
 	public has(key: TKey)
 	{
 		return this.#_entries.has(key);
 	}
 
+	/**
+	 * Removes all keys and values from the cache.
+	 */
 	public clear()
 	{
 		this.#_entries.clear();
 		this.#_order.clear();
 	}
 
+	/**
+	 * Removes the entry associated with the specified key.
+	 * @param key The key to remove.
+	 * @returns `true` if an entry was removed; otherwise, `false`.
+	 */
 	public remove(key: TKey)
 	{
 		const removedFromEntries = this.#_entries.delete(key);
@@ -96,6 +140,10 @@ export class LruCache<TKey extends {}, TValue extends {} | null> implements Map<
 		return removedFromEntries;
 	}
 
+	/**
+	 * Removes the least recently used (LRU) entry from the cache.
+	 * @returns `true` if an entry was removed; otherwise, `false`.
+	 */
 	public removeLru()
 	{
 		const last = this.#_order.last;
@@ -107,6 +155,11 @@ export class LruCache<TKey extends {}, TValue extends {} | null> implements Map<
 		return this.remove(last.value);
 	}
 
+	/**
+	 * Retrieves and removes the entry associated with the specified key.
+	 * @param key The key to retrieve and remove.
+	 * @returns The value associated with the removed key, or `undefined` if not found.
+	 */
 	public getAndRemove(key: TKey)
 	{
 		const entry = this.#_entries.get(key);
@@ -119,6 +172,10 @@ export class LruCache<TKey extends {}, TValue extends {} | null> implements Map<
 		return entry.value;
 	}
 
+	/**
+	 * Retrieves and removes the least recently used (LRU) entry.
+	 * @returns The value associated with the removed LRU entry, or `undefined` if the cache is empty.
+	 */
 	public getAndRemoveLru()
 	{
 		const last = this.#_order.last;
@@ -132,6 +189,12 @@ export class LruCache<TKey extends {}, TValue extends {} | null> implements Map<
 		return entry.value;
 	}
 
+	/**
+	 * Adds a new entry or replaces an existing entry with the specified key and value.
+	 * If the cache exceeds its maximum size, the least recently used entry will be removed.
+	 * @param key The key to add or replace.
+	 * @param value The value to associate with the key.
+	 */
 	public addOrReplace(key: TKey, value: TValue)
 	{
 		if (this.#_ignoreNewEntries)
