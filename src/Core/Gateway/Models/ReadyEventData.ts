6b4b4e1e3856f1dfa2257666cbb27f8e5cb73966
rexaliis <77251557+michironoaware@users.noreply.github.com>
2025-05-04 14:33:30 -0300

    add userId prop to ReadyEventData

--- src/Core/Gateway/Models/ReadyEventData.ts
+++ src/Core/Gateway/Models/ReadyEventData.ts
@@ -7,18 +7,26 @@ import { ThrowHelper } from "../../../Common/ThrowHelper";
 export class ReadyEventData
 {
 	readonly #_sessionId: string;
+	readonly #_userId: string;
 
 	public constructor(data: any)
 	{
 		SealedClassError.throwIfNotEqual(ReadyEventData, new.target);
 		ThrowHelper.TypeError.throwIfNullable(data);
 		ThrowHelper.TypeError.throwIfNotType(data.sessionId, "string");
+		ThrowHelper.TypeError.throwIfNotType(data.userId, "string");
 
 		this.#_sessionId = data.sessionId;
+		this.#_userId = data.userId;
 	}
 
 	public get sessionId()
 	{
 		return this.#_sessionId;
+	}
+
+	public get userId()
+	{
+		return this.#_userId;
 	}
 }
