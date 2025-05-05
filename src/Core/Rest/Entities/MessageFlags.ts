a8d8dc081940b4ae3c40a62b1c8f303fe5b2de50
rexaliis <77251557+michironoaware@users.noreply.github.com>
2025-05-05 19:02:15 -0300

    remove Action message flag

--- src/Core/Rest/Entities/MessageFlags.ts
+++ src/Core/Rest/Entities/MessageFlags.ts
@@ -7,13 +7,6 @@ export const MessageFlags =
 		 * Indicates that the author of the message should be hidden in the UI.
 		 */
 		HideAuthor: 1n,
-
-		/**
-		 * Indicates that the message content describes an action performed by the author.
-		 * When this flag is set, the message should be displayed with the author's display name
-		 * preceding the action text, to clearly attribute the action to the author.
-		 */
-		Action: 2n,
 	} as const;
 
 Object.freeze(MessageFlags);
