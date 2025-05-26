c5d7fc591861dd7f0422c113ef267901525029b5
michironoaware <77251557+michironoaware@users.noreply.github.com>
2025-05-26 19:55:58 -0300

    Create BanIssuerType.ts

---
+++ src/Core/Rest/Entities/BanIssuerType.ts
@@ -0,0 +1,19 @@
+﻿/**
+ * Enumerates the type of entities that can issue a ban within Aula.
+ * */
+export const BanIssuerType =
+	{
+		/*
+		* The system.
+		* */
+		System: 0,
+
+		/**
+		 * An aula user.
+		 * */
+		User: 1,
+	} as const;
+
+Object.freeze(BanIssuerType);
+
+export type BanIssuerType = typeof BanIssuerType[keyof typeof BanIssuerType] | number;
