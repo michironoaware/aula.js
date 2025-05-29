8c8d23076ef645ee681066c99c3ae8a1ce6e9c95
michironoaware <77251557+michironoaware@users.noreply.github.com>
2025-05-29 18:15:14 -0300

    Create RoleCreatedEvent.ts

---
+++ src/Core/Gateway/RoleCreatedEvent.ts
@@ -0,0 +1,43 @@
+﻿import { Role } from "../Rest/Entities/Role";
+import { GatewayClient } from "./GatewayClient";
+import { SealedClassError } from "../../Common/SealedClassError";
+import { ThrowHelper } from "../../Common/ThrowHelper";
+
+/**
+ * Emitted when a role is created.
+ * @sealed
+ * */
+export class RoleCreatedEvent
+{
+	readonly #_role: Role;
+	readonly #_gatewayClient: GatewayClient;
+
+	/**
+	 * @package
+	 * */
+	public constructor(role: Role, gatewayClient: GatewayClient)
+	{
+		SealedClassError.throwIfNotEqual(RoleCreatedEvent, new.target);
+		ThrowHelper.TypeError.throwIfNotType(role, Role);
+		//ThrowHelper.TypeError.throwIfNotType(gatewayClient, GatewayClient); // Circular dependency problem
+
+		this.#_role = role;
+		this.#_gatewayClient = gatewayClient;
+	}
+
+	/**
+	 * Gets the id of the role created.
+	 * */
+	public get role()
+	{
+		return this.#_role;
+	}
+
+	/**
+	 * Gets the {@link GatewayClient} that initialized this instance.
+	 * */
+	public get gatewayClient()
+	{
+		return this.#_gatewayClient;
+	}
+}
