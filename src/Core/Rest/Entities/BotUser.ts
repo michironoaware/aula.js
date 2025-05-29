f177bf1dbbd9e7c56c78fcb2d7dd318a7274825f
michironoaware <77251557+michironoaware@users.noreply.github.com>
2025-05-29 18:30:50 -0300

    Create BotUser.ts

---
+++ src/Core/Rest/Entities/BotUser.ts
@@ -0,0 +1,30 @@
+﻿import { User } from "./User";
+import { UserData } from "./Models/UserData";
+import { RestClient } from "../RestClient";
+import { SealedClassError } from "../../../Common/SealedClassError";
+import { InvalidOperationError } from "../../../Common/InvalidOperationError";
+import { UserType } from "./UserType";
+
+/**
+ * Represents a bot user within Aula.
+ * @sealed
+ * */
+export class BotUser extends User
+{
+	/**
+	 * Initializes a new instance of {@link BotUser}.
+	 * @param data A DTO containing the entity data.
+	 * @param restClient The {@link RestClient} that is initializing this instance.
+	 * @package
+	 * */
+	public constructor(data: UserData, restClient: RestClient)
+	{
+		super(data, restClient);
+		SealedClassError.throwIfNotEqual(BotUser, new.target);
+
+		if (data.type !== UserType.Bot)
+		{
+			throw new InvalidOperationError(`User type expected to be ${UserType.Bot}.`);
+		}
+	}
+}
